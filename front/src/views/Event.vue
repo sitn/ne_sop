e593c9e29c49208ceed02f3479e99d4a06311846
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-14 16:28:02 +0200

    Create Event.vue

---
+++ front/src/views/Event.vue
@@ -0,0 +1,141 @@
+<template>
+    <div class="">
+
+        <!-- Breadcrumbs navigation -->
+        <div class="q-pa-sm q-gutter-sm">
+            <q-breadcrumbs style="font-size: 16px">
+                <q-breadcrumbs-el label="Événements" to="/events" />
+                <q-breadcrumbs-el :label="event.date" />
+            </q-breadcrumbs>
+        </div>
+
+        <!-- Identification -->
+        <FormSection title="Event">
+            <template v-slot:content>
+                <div class="text-h6">Événement</div>
+
+                <div class="row q-col-gutter-lg q-py-md">
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <!--  <q-input type="date" bg-color="white" outlined v-model="eventDate" label="Date" required /> -->
+                        <q-input bg-color="white" outlined v-model="event.date" label="Date" mask="date" :rules="['date']">
+                            <template v-slot:append>
+                                <q-icon name="event" class="cursor-pointer">
+                                    <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                                        <q-date v-model="event.date" color="blue-grey" text-color="white">
+                                            <div class="row items-center justify-end">
+                                                <q-btn v-close-popup label="Close" color="primary" flat />
+                                            </div>
+                                        </q-date>
+                                    </q-popup-proxy>
+                                </q-icon>
+                                <!-- 
+                                <q-icon name="event" class="cursor-pointer">
+                                    <q-popup-proxy>
+                                        <q-date v-model="event.date"></q-date>
+                                    </q-popup-proxy>
+                                </q-icon>
+                                -->
+                            </template>
+                        </q-input>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <!-- <q-input type="time" bg-color="white" outlined v-model="event.time" label="Heure" /> -->
+                        <q-input bg-color="white" outlined v-model="event.time" mask="time" :rules="['time']">
+                            <template v-slot:append>
+                                <q-icon name="access_time" class="cursor-pointer">
+                                    <q-popup-proxy transition-show="scale" transition-hide="scale">
+                                        <q-time v-model="event.time" color="blue-grey" text-color="white">
+                                            <div class="row items-center justify-end">
+                                                <q-btn v-close-popup label="Close" color="primary" flat />
+                                            </div>
+                                        </q-time>
+                                    </q-popup-proxy>
+                                </q-icon>
+                            </template>
+                        </q-input>
+                    </div>
+
+
+
+                </div>
+
+                <div class="row q-col-gutter-lg q-py-md">
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <q-select bg-color="white" outlined v-model="event.eventType" :options="eventTypes" label="Type"
+                            clearable>
+                        </q-select>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <!-- <q-input bg-color="white" outlined v-model="event.itemNumber" label="Objet lié" /> -->
+                        <q-select bg-color="white" outlined v-model="event.itemNumber" :options="items"
+                            option-label="number" option-value="number" label="Objet lié" clearable>
+                            <template v-slot:option="scope">
+                                <q-item v-bind="scope.itemProps">
+                                    <q-item-section>
+                                        <q-item-label>{{ scope.opt.number }} - {{ scope.opt.type }}</q-item-label>
+                                        <q-item-label caption>{{ scope.opt.title }}</q-item-label>
+                                    </q-item-section>
+                                </q-item>
+                            </template>
+                        </q-select>
+                    </div>
+                </div>
+
+                <div class="row q-col-gutter-lg q-py-md">
+                    <div class="col">
+                        <q-input bg-color="white" outlined v-model="event.description" label="Description"
+                            type="textarea" />
+                    </div>
+                </div>
+
+                <div class="bg-light-blue-1 q-my-md q-pa-md">
+                    {{ event }}
+                </div>
+            </template>
+        </FormSection>
+
+    </div>
+</template>
+
+<script>
+import events from '../assets/data/events.json'
+import items from '../assets/data/items.json'
+import FormSection from "../components/FormSection.vue"
+
+
+export default {
+    name: 'Event',
+    components: { FormSection },
+    props: { 'model': Object },
+    emits: [],
+    setup() {
+        return {
+
+            // model: ref(null),
+        }
+    },
+    data() {
+        return {
+            event: events.find(e => e.id === this.$route.params.id),
+            items: items,
+            eventDate: null,
+            eventTime: null,
+            eventTypes: [
+                'Dépôt', 'Demande département', 'Délai', 'Clôture'
+            ],
+        }
+    },
+    computed: {
+    },
+    mounted() {
+
+    },
+    methods: {
+
+
+    }
+}
+</script>
+
+<style scoped></style>
