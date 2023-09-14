39aa708ab75a71e7e944f053811591aba4f0ca0c
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-14 16:27:30 +0200

    Updated paths to components and routes

--- front/src/main.js
+++ front/src/main.js
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import { Quasar } from 'quasar'
 import App from './App.vue'
-import ItemsList from '@/views/ItemsList.vue'
-import EntitiesList from '@/views/EntitiesList.vue'
-import EventsList from '@/views/EventsList.vue'
-import Statistics from '@/views/Statistics.vue'
-import Item from '@/views/Item.vue'
-import Entity from '@/views/Entity.vue'
-import Login from '@/views/Login.vue'
+import ItemsList from './views/ItemsList.vue'
+import EntitiesList from './views/EntitiesList.vue'
+import EventsList from './views/EventsList.vue'
+import Event from './views/Event.vue'
+import Statistics from './views/Statistics.vue'
+import Item from './views/Item.vue'
+import Entity from './views/Entity.vue'
+import Login from './views/Login.vue'
 
 const routes = [
     { path: '/items', name: 'ItemsList', component: ItemsList },
@@ -16,6 +17,7 @@ const routes = [
     { path: '/entities', name: 'EntitiesList', component: EntitiesList },
     { path: '/entities/:id', name: 'Entity', component: Entity, props: true },
     { path: '/events', name: 'EventsList', component: EventsList },
+    { path: '/events/:id', name: 'Event', component: Event, props: true },
     { path: '/statistics', name: 'Statistics', component: Statistics },
     { path: '/login', name: 'Login', component: Login }
 ]
