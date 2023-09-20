12f8bbbf89737b510d659d3b6cbc3d45b69da766
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-20 14:49:18 +0200

    Update Statistics.vue

--- front/src/views/Statistics.vue
+++ front/src/views/Statistics.vue
@@ -1,13 +1,53 @@
 <template>
     <div class="">
 
+        <!-- Breadcrumbs navigation -->
         <div class="q-pa-sm q-gutter-sm">
             <q-breadcrumbs style="font-size: 16px">
                 <q-breadcrumbs-el label="Statistiques" to="/statistics" />
             </q-breadcrumbs>
         </div>
 
-        <h1>Statistiques</h1>
+        <!-- Identification -->
+        <FormSection title="Identification">
+            <template v-slot:content>
+                <div class="text-h6">Statistiques</div>
+
+            </template>
+        </FormSection>
 
     </div>
 </template>
+
+<script>
+import FormSection from "../components/FormSection.vue"
+
+
+export default {
+    name: 'Help',
+    components: { FormSection },
+    props: { 'model': Object },
+    emits: [],
+    setup() {
+        return {
+
+            // model: ref(null),
+        }
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped></style>
