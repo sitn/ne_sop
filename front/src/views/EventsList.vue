04dd8041a91b89b0e2e464b3dd12846fa25b1db1
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-21 17:39:40 +0200

    Added ICS download

--- front/src/views/EventsList.vue
+++ front/src/views/EventsList.vue
@@ -52,9 +52,14 @@
                     <!-- ACTIONS COLUMN -->
                     <q-td key="actions" :props="props">
                         <div class="float-right">
-                            <q-btn dense round flat color="grey" name="calendar" @click="console.log(props.row.date)" icon="sym_o_calendar_add_on">
+                            <q-btn dense round flat color="grey" name="calendar" @click="downloadICS(props.row)" icon="sym_o_calendar_add_on">
                                 <q-tooltip class="bg-black">Ajouter au calendrier</q-tooltip>
                             </q-btn>
+                            <!-- 
+                            <q-btn href="data:,I am text file" download="a122.txt" dense round flat color="grey" name="calendar" @click="" icon="sym_o_calendar_add_on">
+                                <q-tooltip class="bg-black">Ajouter au calendrier</q-tooltip>
+                            </q-btn>
+                            -->
                             <q-btn dense round flat color="red" name="delete" @click="console.log(props.row.id)" icon="sym_o_delete">
                                 <q-tooltip class="bg-black">Supprimer</q-tooltip>
                             </q-btn>
@@ -73,6 +78,9 @@
 <script>
 // import items from '../assets/data/items.json'
 import events from '../assets/data/events.json'
+// import * as ics from 'ics'
+import { createEvent } from 'ics';
+
 
 //console.log(items.map(e => (e.events)).flat())
 
@@ -136,6 +144,95 @@ export default {
 
     },
     methods: {
+
+        async downloadICS(val) {
+
+            console.log('download ICS')
+            console.log(val)
+
+            // const extract = date => val.date.toISOString().split(/[^0-9]/).slice(0, -1)
+            let date = new Date(val.date)
+            console.log(date)
+
+            let dateArray = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+            console.log(dateArray)
+
+            let event = {
+                start: dateArray, // Date.parse(val.date), //  [2018, 5, 30, 6, 30],
+                duration: { hours: 1, minutes: 0 },
+                title: `Objet parlementaire ${val.itemNumber}`,
+                description: '',
+                location: '',
+                /*
+                url: 'http://www.bolderboulder.com/',
+                geo: { lat: 40.0095, lon: 105.2669 },
+                categories: ['10k races', 'Memorial Day Weekend', 'Boulder CO'],
+                status: 'CONFIRMED',
+                busyStatus: 'BUSY',
+                organizer: { name: 'Admin', email: 'Race@BolderBOULDER.com' },
+                attendees: [
+                    { name: 'Adam Gibbons', email: 'adam@example.com', rsvp: true, partstat: 'ACCEPTED', role: 'REQ-PARTICIPANT' },
+                    { name: 'Brittany Seaton', email: 'brittany@example2.org', dir: 'https://linkedin.com/in/brittanyseaton', role: 'OPT-PARTICIPANT' }
+                ]
+                */
+            }
+
+            const filename = 'sop.ics'
+            const file = await new Promise((resolve, reject) => {
+                createEvent(event, (error, value) => {
+                    if (error) {
+                        reject(error)
+                    }
+
+                    resolve(new File([value], filename, { type: 'text/calendar' }))
+                })
+            })
+            const url = URL.createObjectURL(file);
+
+            // trying to assign the file URL to a window could cause cross-site
+            // issues so this is a workaround using HTML5
+            const anchor = document.createElement('a');
+            anchor.href = url;
+            anchor.download = filename;
+
+            document.body.appendChild(anchor);
+            anchor.click();
+            document.body.removeChild(anchor);
+
+            URL.revokeObjectURL(url);
+        },
+        /*
+        downloadICS(val) {
+
+            console.log(val)
+
+            let event = {
+                start: [2018, 5, 30, 6, 30],
+                duration: { hours: 6, minutes: 30 },
+                title: 'Bolder Boulder',
+                description: 'Annual 10-kilometer run in Boulder, Colorado',
+                location: 'Folsom Field, University of Colorado (finish line)',
+                url: 'http://www.bolderboulder.com/',
+                geo: { lat: 40.0095, lon: 105.2669 },
+                categories: ['10k races', 'Memorial Day Weekend', 'Boulder CO'],
+                status: 'CONFIRMED',
+                busyStatus: 'BUSY',
+                organizer: { name: 'Admin', email: 'Race@BolderBOULDER.com' },
+
+            }
+
+            ics.createEvent(event, (error, value) => {
+                if (error) {
+                    console.log(error)
+                    return
+                }
+                window.open("data:text/calendar;charset=utf8," + value);
+                console.log(value)
+            })
+
+        }
+        */
+
     }
 }
 </script>
