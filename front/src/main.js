b77eba6944664ac27ecc1e39cb77edd9d2035e9e
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-26 11:40:00 +0200

    Added "NewItem" route

--- front/src/main.js
+++ front/src/main.js
@@ -8,6 +8,7 @@ import EventsList from './views/EventsList.vue'
 import Event from './views/Event.vue'
 import Statistics from './views/Statistics.vue'
 import Item from './views/Item.vue'
+import NewItem from './views/NewItem.vue'
 import Entity from './views/Entity.vue'
 import Login from './views/Login.vue'
 import Admin from './views/Admin.vue'
@@ -16,6 +17,7 @@ import Help from './views/Help.vue'
 const routes = [
     { path: '/items', name: 'ItemsList', component: ItemsList },
     { path: '/items/:id', name: 'Item', component: Item, props: true },
+    { path: '/items/new', name: 'NewItem', component: NewItem, props: true },
     { path: '/entities', name: 'EntitiesList', component: EntitiesList },
     { path: '/entities/:id', name: 'Entity', component: Entity, props: true },
     { path: '/events', name: 'EventsList', component: EventsList },
