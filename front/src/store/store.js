cf07f35385d8663864bd012432a41f75071eaa5e
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-28 16:53:57 +0200

    Update store.js

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,6 +1,12 @@
 // store.js
 import { reactive } from 'vue'
+import entities from '../assets/data/entities.json'
+import items from '../assets/data/items.json'
+import events from '../assets/data/events.json'
 
 export const store = reactive({
-    saveButton: false
+    dev: true,
+    entities: entities,
+    items: items,
+    events: events,
 })
