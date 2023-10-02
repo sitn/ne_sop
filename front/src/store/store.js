ed87b5b21c930971e2b3a166461c55243c1c1c94
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-02 17:37:41 +0200

    Changed events source

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -2,11 +2,13 @@
 import { reactive } from 'vue'
 import entities from '../assets/data/entities.json'
 import items from '../assets/data/items.json'
-import events from '../assets/data/events.json'
+// import events from '../assets/data/events.json'
+import users from '../assets/data/users.json'
 
 export const store = reactive({
     dev: true,
     entities: entities,
     items: items,
-    events: events,
+    events: items.map((x) => (x.events)).flat(1),
+    users: users
 })
