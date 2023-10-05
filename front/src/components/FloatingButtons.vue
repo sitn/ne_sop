1e438c191bd002de24404a4f60859ca113416d68
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-05 17:34:37 +0200

    Added delete button in component

--- front/src/components/FloatingButtons.vue
+++ front/src/components/FloatingButtons.vue
@@ -1,19 +1,30 @@
 <template>
     <!-- FLOATING ACTION BUTTONS -->
-    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-page-sticky position="bottom-right" :offset="[18, 18]" class="justify-center items-center">
 
-        <!-- SAVE BUTTON -->
-        <div class="q-py-xs">
-            <q-btn :loading="wait" fab icon="sym_o_save" color="amber-14" @click="save()" v-if="editMode">
-                <q-tooltip class="bg-black">Enregistrer</q-tooltip>
-            </q-btn>
-        </div>
+        <div class="column items-center q-py-xs">
 
-        <!-- EDIT/VIEW MODE BUTTON -->
-        <div class="q-py-xs">
-            <q-btn fab :icon="editMode ? 'sym_o_visibility' : 'sym_o_edit'" color="blue" @click="switchEdit()">
-                <q-tooltip class="bg-black">{{ editMode ? 'Visualisation' : 'Modification' }}</q-tooltip>
-            </q-btn>
+            <!-- SAVE BUTTON -->
+            <div class="q-py-xs">
+                <q-btn :loading="wait" fab icon="sym_o_save" color="green-5" @click="save()" v-if="editMode & buttons.save">
+                    <q-tooltip class="bg-black">Enregistrer</q-tooltip>
+                </q-btn>
+            </div>
+
+            <!-- DELETE BUTTON -->
+            <div class="q-py-xs">
+                <q-btn fab icon="sym_o_delete_forever" color="red" @click="remove()" v-if="editMode & buttons.deletion">
+                    <q-tooltip class="bg-black">Supprimer</q-tooltip>
+                </q-btn>
+            </div>
+
+            <!-- EDIT/VIEW MODE BUTTON -->
+            <div class="q-py-xs">
+                <q-btn fab :icon="editMode ? 'sym_o_visibility' : 'sym_o_edit'" color="blue" @click="switchEdit()">
+                    <q-tooltip class="bg-black">{{ editMode ? 'Visualisation' : 'Modification' }}</q-tooltip>
+                </q-btn>
+            </div>
+
         </div>
 
     </q-page-sticky>
@@ -24,8 +35,8 @@
 export default {
     name: 'FloatingButtons',
     components: {},
-    props: { 'edit': Boolean, 'wait': Boolean },
-    emits: ['saveEvent', 'editEvent'],
+    props: { 'edit': Boolean, 'wait': Boolean, 'buttons': Object },
+    emits: ['saveEvent', 'editEvent', 'deleteEvent'],
     setup() {
         return {
 
@@ -45,13 +56,17 @@ export default {
     methods: {
         switchEdit() {
             this.editMode = !this.editMode
-            console.log(`FloatingButtons.vue | editEvent(${this.editMode})`)
+            console.log(`${this.$options.name}.vue | editEvent(${this.editMode})`)
             this.$emit('editEvent', this.editMode)
 
         },
         save() {
-            console.log('FloatingButtons.vue | saveEvent')
+            console.log(`${this.$options.name}.vue | saveEvent`)
             this.$emit('saveEvent')
+        },
+        remove() {
+            console.log(`${this.$options.name}.vue | deleteEvent`)
+            this.$emit('deleteEvent')
         }
     }
 }
