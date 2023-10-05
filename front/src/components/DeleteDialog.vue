d1e91167b8549742606a7152133c40113b063f35
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-05 17:34:46 +0200

    Created delete dialog

---
+++ front/src/components/DeleteDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- DELETE DIALOG -->
+  <q-dialog :model-value="modelValue" @hide="close()">
+    <q-card>
+
+      <q-card-section class="row items-center">
+        <q-avatar icon="sym_o_delete_forever" color="red" text-color="white" />
+        <span class="q-ml-sm">Supprimer définitivement cette entrée et tous les éléments liés?</span>
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Annuler" color="primary" v-close-popup />
+        <q-btn flat label="Supprimer" color="red" @click="remove" v-close-popup />
+      </q-card-actions>
+
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'DeleteDialog',
+  props: { 'modelValue': Boolean },
+  emits: ['update:modelValue', 'deleteEvent'],
+  methods: {
+    remove() {
+      this.$emit('deleteEvent')
+    },
+    close() {
+      this.$emit('update:modelValue', false)
+    }
+  }
+}
+</script>
+
