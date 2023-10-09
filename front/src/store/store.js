8642ed033e2278a933cfdeb5dd979135fe2ada4e
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-09 17:38:11 +0200

    Changed events attribute source

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -2,7 +2,7 @@
 import { reactive } from 'vue'
 import entities from '../assets/data/entities.json'
 import items from '../assets/data/items.json'
-// import events from '../assets/data/events.json'
+import events from '../assets/data/events.json'
 import users from '../assets/data/users.json'
 
 export const store = reactive({
@@ -10,7 +10,7 @@ export const store = reactive({
     session: { "user": "Test user" },
     entities: entities,
     items: items,
-    events: items.map((x) => (x.events)).flat(1),
+    events: events, // items.map((x) => (x.events)).flat(1),
     documents: items.map((x) => (x.documents)).flat(1),
     attachements: items.map((x) => (x.attachements)).flat(1),
     users: users,
