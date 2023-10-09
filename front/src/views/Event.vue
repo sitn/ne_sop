f386b367c769455fbd7cdb045854e26cfaad5c34
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-09 12:19:31 +0200

    Integrated EventForm component

--- front/src/views/Event.vue
+++ front/src/views/Event.vue
@@ -11,114 +11,15 @@
                 </q-breadcrumbs>
             </div>
 
-            <Form title="Événement" :edit="false" :toggle="true" @edit-event="setEditMode">
-
-                <template v-slot:body>
-
-                    <!-- IDENTIFICATION SECTION -->
-                    <FormSection title="Event">
-                        <template v-slot:content>
-                            <div class="text-h6">Événement</div>
-
-                            <div class="row q-col-gutter-lg q-py-md">
-
-                                <!-- DATE INPUT FIELD -->
-                                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                    <!--  <q-input type="date" bg-color="white" outlined v-model="eventDate" label="Date" required /> -->
-                                    <q-input bg-color="white" outlined v-model="event.date" label="Date" mask="date" :rules="['date']" :disable="!edit">
-                                        <template v-slot:append>
-                                            <q-icon name="event" class="cursor-pointer">
-                                                <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                                                    <q-date v-model="event.date" color="blue-grey" text-color="white">
-                                                        <div class="row justify-end">
-                                                            <q-btn v-close-popup label="Close" color="primary" flat />
-                                                        </div>
-                                                    </q-date>
-                                                </q-popup-proxy>
-                                            </q-icon>
-                                            <!-- 
-                                <q-icon name="event" class="cursor-pointer">
-                                    <q-popup-proxy>
-                                        <q-date v-model="event.date"></q-date>
-                                    </q-popup-proxy>
-                                </q-icon>
-                                -->
-                                        </template>
-                                    </q-input>
-                                </div>
-
-                                <!-- TIME INPUT FIELD -->
-                                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                    <!-- <q-input type="time" bg-color="white" outlined v-model="event.time" label="Heure" /> -->
-                                    <q-input bg-color="white" outlined v-model="event.time" label="Heure" mask="time" :rules="['time']" :disable="!edit">
-                                        <template v-slot:append>
-                                            <q-icon name="access_time" class="cursor-pointer">
-                                                <q-popup-proxy transition-show="scale" transition-hide="scale">
-                                                    <q-time v-model="event.time" color="blue-grey" text-color="white">
-                                                        <div class="row items-center justify-end">
-                                                            <q-btn v-close-popup label="Close" color="primary" flat />
-                                                        </div>
-                                                    </q-time>
-                                                </q-popup-proxy>
-                                            </q-icon>
-                                        </template>
-                                    </q-input>
-                                </div>
-
-                            </div>
-
-                            <div class="row q-col-gutter-lg">
-
-                                <!-- TYPE SELECT FIELD -->
-                                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                    <q-select bg-color="white" outlined v-model="event.eventType" :options="eventTypes" label="Type" clearable :disable="!edit">
-                                    </q-select>
-                                </div>
-
-                                <!-- LINKED ITEM SELECT FIELD -->
-                                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                    <!-- <q-input bg-color="white" outlined v-model="event.itemNumber" label="Objet lié" /> -->
-                                    <q-select bg-color="white" outlined v-model="event.itemNumber" :options="this.items" option-label="number" option-value="number" label="Objet lié" @update:model-value="setLinkedItem()" emit-value clearable :disable="!edit">
-                                        <template v-slot:option="scope">
-                                            <q-item v-bind="scope.itemProps">
-                                                <q-item-section>
-                                                    <q-item-label>{{ scope.opt.number }} - {{ scope.opt.type }}</q-item-label>
-                                                    <q-item-label caption>{{ scope.opt.title }}</q-item-label>
-                                                </q-item-section>
-                                            </q-item>
-                                        </template>
-                                    </q-select>
-                                </div>
-                            </div>
-
-                            <!-- DESCRIPTION TEXT AREA FIELD -->
-                            <div class="row q-col-gutter-lg q-my-sm">
-                                <div class="col">
-                                    <q-input bg-color="white" outlined v-model="event.description" label="Description" type="textarea" :disable="!edit" />
-                                </div>
-                            </div>
-
-                            <!-- TODO REMOVE/DEV DISPLAY JSON-->
-                            <div class="bg-light-blue-1 q-my-md q-pa-md" v-if="store.dev">
-                                <div>event</div>
-                                <div>{{ event }}</div>
-                            </div>
-
-                            <!-- TODO REMOVE/DEV DISPLAY JSON-->
-                            <div class="bg-light-blue-1 q-my-md q-pa-md" v-if="store.dev">
-                                <div>store.event</div>
-                                <div>{{ store.events.find(e => e.id === this.$route.params.id) }}</div>
-                            </div>
-
-                        </template>
-                    </FormSection>
-
-                </template>
-
-            </Form>
+            <!-- FORM -->
+            <EventForm v-model="event" :edit="edit"></EventForm>
 
             <!-- FLOATING ACTION BUTTONS -->
             <FloatingButtons :edit="false" :wait="wait" :buttons="{ 'save': true, 'deletion': true }" @save-event="save" @delete-event="handleDeletion" @edit-event="setEditMode"></FloatingButtons>
+
+            <!-- DELETE DIALOG -->
+            <DeleteDialog v-model="dialog.deletion" @delete-event="remove" />
+
 
         </q-layout>
     </div>
@@ -128,13 +29,13 @@
 import { store } from '../store/store.js'
 import { sleep } from '../store/shared.js'
 import eventTypes from '../assets/data/event-types.json'
-import Form from "../components/Form.vue"
-import FormSection from "../components/FormSection.vue"
 import FloatingButtons from "../components/FloatingButtons.vue"
+import DeleteDialog from '../components/DeleteDialog.vue'
+import EventForm from "../components/EventForm.vue"
 
 export default {
     name: 'Event',
-    components: { Form, FormSection, FloatingButtons },
+    components: { FloatingButtons, DeleteDialog, EventForm },
     props: {},
     emits: [],
     setup() {
@@ -144,6 +45,7 @@ export default {
     data() {
         return {
             store,
+            dialog: { deletion: false },
             edit: false,
             wait: false,
             event: null, // store.events.find(e => e.id === this.$route.params.id),
@@ -177,6 +79,15 @@ export default {
                 }
             })
             this.wait = false
+        },
+        handleDeletion() {
+            this.dialog.deletion = true
+        },
+        async remove() {
+            // TODO: DELETE RECORD IN DATABASE
+            console.log(`${this.$options.name}.vue | remove()`)
+            // store.entities.splice(this.index, 1)
+            this.$router.push({ name: 'EventsList' })
         },
         setEditMode(val) {
             console.log(`${this.$options.name}.vue | setEditMode(${val})`)
