6e1c3dcde241b3deb8df0a76a8078f8eab57fe3b
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-10 12:41:39 +0200

    Updated Floating buttons control flow and symbology

--- front/src/components/FloatingButtons.vue
+++ front/src/components/FloatingButtons.vue
@@ -6,21 +6,21 @@
 
             <!-- SAVE BUTTON -->
             <div class="q-py-xs">
-                <q-btn :loading="wait" fab icon="sym_o_save" color="green-5" @click="save()" v-if="editMode & buttons.save">
+                <q-btn :loading="wait" fab icon="sym_o_save" color="green-5" @click="save()" v-if="editMode & saveButton.render" :disabled="saveButton.disable">
                     <q-tooltip class="bg-black">Enregistrer</q-tooltip>
                 </q-btn>
             </div>
 
             <!-- DELETE BUTTON -->
             <div class="q-py-xs">
-                <q-btn fab icon="sym_o_delete_forever" color="red" @click="remove()" v-if="editMode & buttons.deletion">
+                <q-btn fab icon="sym_o_delete_forever" color="red" @click="remove()" v-if="editMode & deleteButton.render" :disabled="deleteButton.disable">
                     <q-tooltip class="bg-black">Supprimer</q-tooltip>
                 </q-btn>
             </div>
 
             <!-- EDIT/VIEW MODE BUTTON -->
             <div class="q-py-xs">
-                <q-btn fab :icon="editMode ? 'sym_o_visibility' : 'sym_o_edit'" color="blue" @click="switchEdit()">
+                <q-btn fab :icon="editMode ? 'sym_o_close' : 'sym_o_edit'" color="blue" @click="switchEdit()">
                     <q-tooltip class="bg-black">{{ editMode ? 'Visualisation' : 'Modification' }}</q-tooltip>
                 </q-btn>
             </div>
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+import { store } from '../store/store.js'
 
 export default {
     name: 'FloatingButtons',
@@ -44,21 +45,43 @@ export default {
     },
     data() {
         return {
+            // saveButton: {}, // { render: ['active', 'disable'].includes(this.buttons.save), disable: this.buttons.save === 'disable' },
+            // deleteButton: {},// { render: ['active', 'disable'].includes(this.buttons.deletion), disable: this.buttons.deletion === 'disable' },
             waitMode: this.wait,
             editMode: this.edit
         }
     },
     computed: {
+        saveButton() { return { render: ['active', 'disable'].includes(this.buttons.save), disable: !store.valid } },
+        deleteButton() { return { render: ['active', 'disable'].includes(this.buttons.deletion), disable: this.buttons.deletion === 'disable' } }
     },
     mounted() {
 
     },
+    watch: {
+        /*
+        buttons: {
+            handler(newValue, oldValue) {
+                console.log(`${this.$options.name}.vue | watch buttons`)
+                this.saveButton = { render: ['active', 'disable'].includes(this.buttons.save), disable: this.buttons.save === 'disable' }
+                this.deleteButton = { render: ['active', 'disable'].includes(this.buttons.deletion), disable: this.buttons.deletion === 'disable' }
+
+                console.log(`${this.$options.name}.vue | saveButton`)
+                console.log(`${this.$options.name}.vue | buttons`)
+                console.log(this.buttons)
+                console.log(this.saveButton)
+                console.log(oldValue)
+                console.log(newValue)
+            },
+            deep: true
+        }
+        */
+    },
     methods: {
         switchEdit() {
-            this.editMode = !this.editMode
             console.log(`${this.$options.name}.vue | editEvent(${this.editMode})`)
+            this.editMode = !this.editMode
             this.$emit('editEvent', this.editMode)
-
         },
         save() {
             console.log(`${this.$options.name}.vue | saveEvent`)
