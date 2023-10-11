b512d47c8594f8d05ab9fd4721873f1fa87dcac8
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-11 17:34:58 +0200

    Added/updated validation functions

--- front/src/store/shared.js
+++ front/src/store/shared.js
@@ -4,16 +4,18 @@ export default {
 }
 */
 import parsePhoneNumber from 'libphonenumber-js'
+import { date } from 'quasar'
 
 export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-export const checkPhoneNumber = val => {
+export const checkPhoneNumber = (val, allowEmpty = true) => {
+
+    if (allowEmpty && val === '') {
+        return true
+    }
 
     let phoneNumber = parsePhoneNumber(val)
     if (phoneNumber) {
-        val = '+41255555555'
-        console.log('this.entity.phoneNumber')
-        console.log(this.entity.phoneNumber)
         return phoneNumber.isPossible() & phoneNumber.isValid()
     } else {
         return 'Format non-valable'
@@ -21,10 +23,10 @@ export const checkPhoneNumber = val => {
 
 }
 
-export const checkEmail = val => {
+export const checkEmail = (val, allowEmpty = true) => {
 
     let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
-    let isValid = reg.test(val)
+    let isValid = reg.test(val) || (allowEmpty && val === '')
 
     if (isValid) {
         return true
@@ -34,10 +36,10 @@ export const checkEmail = val => {
 
 }
 
-export const checkWebsite = val => {
+export const checkWebsite = (val, allowEmpty = true) => {
 
     let reg = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
-    let isValid = reg.test(val)
+    let isValid = reg.test(val) || (allowEmpty && val === '')
 
     if (isValid) {
         return true
@@ -45,14 +47,22 @@ export const checkWebsite = val => {
         return 'Format non-valable'
     }
 
+}
+
+export const formatPhoneNumber = val => {
+    // this.entity.telephone = '+41255555555'
+    console.log('formatPhoneNumber')
+    console.log(val)
+}
+
+export const checkDate = val => {
+    let mydate = date.extractDate(val, 'DD.MM.YYYY')
+    console.log(val)
+    console.log(mydate)
+    console.log(date.isValid(mydate))
+    return date.isValid(val) ? true : 'Format non-valable'
 }
 
 export const checkFilled = val => {
-    return val ? true : 'Choix non-valable'
+    return val ? true : 'Champ obligatoire'
 }
-
-/*
-checkFilled(val) { 
-            
-},
-*/
