8fc7317eb9e9a62353a01e42a58dcc5a16fe2962
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-11 17:37:46 +0200

    Update NewEventDialog.vue

--- front/src/views/NewEventDialog.vue
+++ front/src/views/NewEventDialog.vue
@@ -6,111 +6,27 @@
 
         <q-card-section class="row items-center scroll" style="max-height: 70vh">
 
-            <div class="col">
-
-                <!-- Identification -->
-                <FormSection title="Identification">
-                    <template v-slot:content>
-                        <div class="text-h6">Identification</div>
-
-                        <div class="row q-col-gutter-lg q-py-md">
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input bg-color="white" outlined v-model="entity.name" label="Nom" :disable="!edit" />
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <!-- TYPE SELECT FIELD -->
-                                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                    <q-select bg-color="white" outlined v-model="entity.type" :options="entityTypes" label="Type" :disable="!edit">
-                                    </q-select>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row q-col-gutter-lg q-py-md">
-                            <div class="col">
-                                <q-input bg-color="white" outlined v-model="entity.description" label="Description" type="textarea" :disable="!edit" />
-                            </div>
-                        </div>
-
-                        <div class="bg-light-blue-1 q-my-md q-pa-md">
-                            {{ entity }}
-                        </div>
-                    </template>
-                </FormSection>
-
-                <!-- Address -->
-                <FormSection title="Addresse">
-                    <template v-slot:content>
-                        <div class="text-h6">Addresse</div>
-
-                        <div class="row q-col-gutter-lg q-py-md">
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input bg-color="white" outlined v-model="entity.street" label="Rue" :disable="!edit" />
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input bg-color="white" outlined v-model="entity.postalCode" label="Code postal" :disable="!edit" />
-                            </div>
-                        </div>
-
-                        <div class="row q-col-gutter-lg q-py-md">
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input bg-color="white" outlined v-model="entity.city" label="Localité" :disable="!edit" />
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input bg-color="white" outlined v-model="entity.region" label="Canton / Région" :disable="!edit" />
-                            </div>
-                        </div>
-                        <div class="row q-col-gutter-lg q-py-md">
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input bg-color="white" outlined v-model="entity.country" label="Pays" :disable="!edit" />
-                            </div>
-
-                        </div>
-
-                    </template>
-                </FormSection>
-
-                <FormSection title="Contact">
-                    <template v-slot:content>
-                        <div class="text-h6">Contact</div>
-
-                        <div class="row q-col-gutter-lg q-py-md">
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input type="email" bg-color="white" outlined v-model="entity.email" label="Email" :rules="[val => checkEmail(val)]" :disable="!edit" />
-                            </div>
-                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                                <q-input type="tel" bg-color="white" outlined v-model="entity.telephone" label="Téléphone (format international)" @update:model-value="checkPhoneNumber(this.entity.telephone)" @blur="formatPhoneNumber(this.entity.telephone)" :rules="[val => checkPhoneNumber(val)]" :disable="!edit" />
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col">
-                                <q-input type="text" bg-color="white" outlined v-model="entity.website" label="Site Internet" :rules="[val => checkWebsite(val)]" :disable="!edit" />
-                            </div>
-                        </div>
-
-                    </template>
-                </FormSection>
-
-            </div>
-
+            <!-- FORM -->
+            <EventForm v-model="event" :edit="edit"></EventForm>
+            <!-- <EventForm v-model="event" :edit="edit" @validation-event="handleValidation"></EventForm> -->
         </q-card-section>
 
         <q-card-actions align="right">
             <q-btn flat label="Annuler" color="primary" v-close-popup />
-            <q-btn flat label="Sauvegarder" color="primary" @click="save()" v-close-popup />
+            <q-btn flat label="Sauvegarder" color="primary" @click="save()" v-close-popup :disable="!event.valid" />
         </q-card-actions>
     </q-card>
 </template>
 
 <script>
+import { v4 as uuidv4 } from 'uuid'
 import { store } from '../store/store.js'
-import FormSection from "../components/FormSection.vue"
+import EventForm from "../components/EventForm.vue"
 
 export default {
     name: 'NewEventDialog',
-    components: { FormSection },
-    props: { 'model': Object },
+    components: { EventForm },
+    props: { 'itemId': String, 'modelValue': Object },
     emits: ['addNewEvent'],
     setup() {
         return {
@@ -120,18 +36,42 @@ export default {
         return {
             store,
             edit: true,
-            event: {}
+            valid: false,
+            event: {
+                "id": uuidv4(),
+                "itemId": "",
+                "itemNumber": "",
+                "itemName": "",
+                "itemType": "",
+                "eventType": "",
+                "date": "",
+                "time": "",
+                "status": "",
+                "statusGrandConseil": "",
+                "description": "",
+                "valid": false
+            }
         }
     },
     computed: {
+        item: {
+            get() {
+                return this.modelValue
+            },
+            set(item) {
+                this.$emit('update:modelValue', item)
+            }
+        }
     },
     mounted() {
 
     },
     methods: {
-        save(val) {
+        save() {
 
-
+            // store.events.push(Object.assign({}, this.event))
+            this.item.events.push(Object.assign({}, this.event))
+            // store.items[1].events.push(Object.assign({}, this.event))
         },
 
     }
