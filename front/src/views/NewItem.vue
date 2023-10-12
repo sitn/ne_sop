e81db66736d9dc2e684d3d728e02c958ad7cd04c
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-12 17:55:31 +0200

    Removed unused code

--- front/src/views/NewItem.vue
+++ front/src/views/NewItem.vue
@@ -14,61 +14,6 @@
             <!-- FORM -->
             <ItemForm v-model="item" :edit="edit"></ItemForm>
 
-            <!-- Dialog for creating first model in documents -->
-            <q-dialog v-model="showAddFirstDocumentDialog">
-                <q-card>
-                    <q-card-section>
-                        <div class="text-h6">Ajouter un nouveau modèle aux documents ?</div>
-                    </q-card-section>
-
-                    <q-card-section class="row q-pt-sm">
-                        <div class="row">
-                            <div class="col-1" style="text-align: center;">
-                                <q-icon name="warning" color="warning" size="3em" />
-                            </div>
-                            <div class="col-11 q-pl-md">
-                                <span class="q-ml-sm" style="float: left;">En cliquant sur confirmer, une version originale du modèle "<b>{{ formalDocumentModels.filter(e => e.id == selectedFormalDocumentModel)[0].name }}</b>" sera ajoutée aux documents.</span>
-                            </div>
-                        </div>
-                    </q-card-section>
-
-                    <q-card-actions align="right">
-                        <q-btn flat label="Annuler" color="primary" v-close-popup />
-                        <q-btn flat label="Confirmer" color="primary" v-close-popup @click="saveNewFirstDocument" />
-                    </q-card-actions>
-                </q-card>
-            </q-dialog>
-
-            <!-- Dialog for adding new version of document -->
-            <q-dialog v-model="showAddDocumentDialog" persistent>
-                <q-card style="min-width: 600px">
-                    <q-card-section>
-                        <div class="text-h6">Ajouter un document</div>
-                    </q-card-section>
-
-                    <q-card-section class="q-pt-sm q-gutter-sm">
-                        <p>
-                            Sélectionner le fichier Word à enregistrer. <br>
-                            Il sera enregistré comme une nouvelle version du modèle "<b>{{ formalDocumentModels.filter(e => e.id == selectedFormalDocumentModel)[0].name }}</b>". Pour changer le type de modèle, il faut modifier la valeur sélectionnée dans le champ "Modèle de document".
-                        </p>
-                        <q-file outlined v-model="newFile" label="Fichier" accept=".doc, .docx" @update:model-value="updatenewFile">
-
-                            <template v-slot:prepend>
-                                <q-icon name="attach_file" />
-                            </template>
-                            <template v-if="newFile" v-slot:append>
-                                <q-icon name="cancel" @click.stop.prevent="newFile = null" class="cursor-pointer" />
-                            </template>
-                        </q-file>
-                    </q-card-section>
-
-                    <q-card-actions align="right" class="text-primary">
-                        <q-btn flat label="Annuler" v-close-popup />
-                        <q-btn flat label="Enregistrer" v-close-popup :disable="newFile === null" @click="saveNewDocument" />
-                    </q-card-actions>
-                </q-card>
-            </q-dialog>
-
             <!-- FLOATING ACTION BUTTONS -->
             <FloatingButtons :edit="edit" :wait="wait" :buttons="actionButtons" @save-event="save" @edit-event="setEditMode"></FloatingButtons>
 
@@ -83,35 +28,8 @@ import { store } from '../store/store.js'
 import { sleep } from '../store/shared.js'
 import documents from '../assets/data/documents.json'
 import itemTypes from '../assets/data/item-types.json'
-import templates from '../assets/data/templates.json'
 import ItemForm from "../components/ItemForm.vue"
 import FloatingButtons from "../components/FloatingButtons.vue"
-
-const formalDocumentColumns = [
-    { name: 'version', align: 'center', label: 'version', field: 'version', sortable: true },
-    { name: 'filename', align: 'left', label: 'Fichier', field: 'filename', sortable: true },
-    { name: 'filesize', align: 'left', label: 'Taille', field: 'filesize', sortable: true },
-    { name: 'auteur', align: 'left', label: 'Auteur', field: 'author', sortable: true },
-    { name: 'date', align: 'left', label: 'Date', field: 'date', sortable: true },
-    { name: 'note', align: 'left', label: 'Note', field: 'note', sortable: true },
-    { name: 'action', align: 'left', label: '', field: 'action' }
-]
-const attachementColumns = [
-    { name: 'filename', align: 'left', label: 'Fichier', field: 'filename', sortable: true },
-    { name: 'filesize', align: 'left', label: 'Taille', field: 'filesize', sortable: true },
-    { name: 'format', align: 'left', label: 'Format', field: 'format', sortable: true },
-    { name: 'auteur', align: 'left', label: 'Auteur', field: 'author', sortable: true },
-    { name: 'date', align: 'left', label: 'Date', field: 'date', sortable: true },
-    { name: 'note', align: 'left', label: 'Note', field: 'note', sortable: true },
-    { name: 'action', align: 'left', label: '', field: 'action' }
-]
-
-const eventsColumns = [
-    { name: "date", align: "left", label: "Date", field: "date", sortable: true },
-    { name: "time", align: "left", label: "Heure", field: "", sortable: true },
-    { name: "type", align: "left", label: "Type", field: "eventType", sortable: true },
-    { name: "actions", align: "center", label: "", field: "", sortable: false }
-]
 
 const subset = ["Parlementaire", "Groupe parlementaire", "Autre"]
 const authors = store.entities.filter(e => subset.includes(e.type))
@@ -128,7 +46,6 @@ export default {
     data() {
         return {
             store,
-            // actionButtons: { save: 'active', deletion: 'none' },
             edit: true,
             wait: false,
             item: {
@@ -157,20 +74,9 @@ export default {
                 "valid": false
             },
             itemTypes: itemTypes,
-            formalDocumentRowsUnfiltered: documents.filter((e) => (e.ressourcetype === 'formal')),
-            formalDocumentRows: [],
-            formalDocumentColumns: formalDocumentColumns,
-            attachementRows: documents.filter((e) => (e.ressourcetype === 'attachement')),
-            attachementColumns: attachementColumns,
-            eventsColumns: eventsColumns,
             authorOptions: authors, //entities.filter(e => e.type.includes("Parlementaire","Groupe politique","Commission parlementaire instituée")),
             addEntityDialog: false,
-            serviceOptions: store.entities.filter((e) => (e.type === "Service de l'état")),
-            formalDocumentModels: templates,
-            selectedFormalDocumentModel: -1,
-            showAddDocumentDialog: false,
-            newFile: null,
-            showAddFirstDocumentDialog: false,
+            serviceOptions: store.entities.filter((e) => (e.type === "Service de l'état"))
         }
     },
     computed: {
@@ -186,11 +92,6 @@ export default {
     created() {
     },
     mounted() {
-
-        this.formalDocumentRows = this.formalDocumentRowsUnfiltered;
-        this.updateFormalDocumentModelFilter(this.selectedFormalDocumentModel);
-        this.updateNumberOfFormalDocumentsByModel();
-
     },
     methods: {
         async save() {
@@ -244,69 +145,12 @@ export default {
             }
 
         },
-
         filterFn(val, update, abort) {
             update(() => {
                 const needle = val.toLowerCase()
                 this.authorOptions = authors.filter(v => v.name.toLowerCase().indexOf(needle) > -1)
             })
-        },
-
-        async downloadRessource(ressource) {
-            let filepath = [
-                import.meta.env.VITE_OP_PATH,
-                '20' + this.item.number.split('.')[0],
-                this.item.number.split('.')[1],
-                ressource.filename
-            ].join('\\')
-
-            console.log('filepath', 'file:///' + filepath)
-            // window.open('file://' +  filepath, "_blank");
-        },
-
-        updateFormalDocumentModelFilter(value) {
-            if (value < 0) {
-                this.formalDocumentRows = this.formalDocumentRowsUnfiltered;
-            } else {
-                this.formalDocumentRows = this.formalDocumentRowsUnfiltered.filter(e => e.model_id == value);
-            }
-        },
-
-        updateNumberOfFormalDocumentsByModel() {
-            this.formalDocumentModels.forEach(e => {
-                e.nbVersions = this.formalDocumentRowsUnfiltered.filter(x => x.model_id == e.id).length;
-            });
-
-            this.formalDocumentModels.sort((a, b) => (a.nbVersions < b.nbVersions) ? 1 : -1);
-
-            this.formalDocumentModels.unshift({
-                id: -1,
-                nbVersions: this.formalDocumentRowsUnfiltered.length,
-                name: "Tous",
-            });
-        },
-
-        updatenewFile(filepath) {
-            console.log('updated new file path', filepath);
-        },
-
-        saveNewDocument() {
-            console.log('save new version', this.newFile)
-        },
-
-        saveNewFirstDocument() {
-            console.log('save new first version document', this.formalDocumentModels.filter(e => e.id == this.selectedFormalDocumentModel)[0].name);
-        },
-
-        openAddNewFormalDocumentDialog() {
-            let nbModelVersions = this.formalDocumentRowsUnfiltered.filter(e => e.model_id == this.selectedFormalDocumentModel).length;
-
-            if (nbModelVersions === 0) {
-                this.showAddFirstDocumentDialog = true;
-            } else if (nbModelVersions > 0) {
-                this.showAddDocumentDialog = true;
-            }
-        },
+        }
     }
 }
 </script>
