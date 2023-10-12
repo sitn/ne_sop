7ad890947bc6da3605b2f1e633ba904f6f3656d0
Matthew Parkan <matthew.parkan@gmail.com>
2023-10-12 12:03:57 +0200

    Added event creation and deletion functionality directly in component

--- front/src/components/EventsTable.vue
+++ front/src/components/EventsTable.vue
@@ -1,6 +1,17 @@
 <template>
+    <div class="row q-px-none q-my-md items-center">
+
+        <!-- ADD NEW EVENT BUTTON -->
+        <div class="col-xs-12 col-sm-4 col-md-6 col-lg-6">
+            <q-btn padding="sm md" unelevated no-caps color="blue-grey-8" text-color="white" icon="sym_o_add_circle" label="Ajouter" @click="addEvent()" :disable="!edit">
+                <q-tooltip class="bg-black">Ajouter un évenement au calendrier</q-tooltip>
+            </q-btn>
+        </div>
+
+    </div>
+
     <!-- EVENTS TABLE -->
-    <q-table :rows="events" :columns="eventsColumns" row-key="date" class="q-my-md">
+    <q-table :rows="events" :columns="columns" row-key="date" class="q-my-md">
         <template v-slot:body="props">
             <q-tr :props="props">
                 <q-td key="date" :props="props">
@@ -13,9 +24,14 @@
                     {{ props.row.eventType }}
                 </q-td>
                 <q-td key="actions" :props="props">
-                    <q-btn dense round flat color="red" name="delete" @click="console.log(props.row)" icon="sym_o_delete" :disable="!edit">
-                        <q-tooltip class="bg-black">Supprimer</q-tooltip>
-                    </q-btn>
+                    <div class="float-right">
+                        <q-btn dense round flat color="grey" name="calendar" @click="downloadICS(props.row)" icon="sym_o_calendar_add_on" :disable="!edit">
+                            <q-tooltip class="bg-black">Ajouter au calendrier</q-tooltip>
+                        </q-btn>
+                        <q-btn dense round flat color="red" name="delete" @click="handleDeletion(props.row.id)" icon="sym_o_delete" :disable="!edit">
+                            <q-tooltip class="bg-black">Supprimer</q-tooltip>
+                        </q-btn>
+                    </div>
                 </q-td>
             </q-tr>
         </template>
@@ -24,14 +40,23 @@
     <div class="bg-light-blue-1 q-my-md q-pa-md" v-if="store.dev">
         {{ events }}
     </div>
-</template>
 
+    <!-- ADD NEW EVENT DIALOG -->
+    <q-dialog v-model="dialog.newEvent">
+        <NewEventDialog v-model="events"></NewEventDialog>
+    </q-dialog>
+
+    <!-- DELETE EVENT DIALOG -->
+    <DeleteDialog v-model="dialog.deletion" @delete-event="remove" />
+</template>
 
 <script>
 import { store } from '../store/store.js'
+import { downloadICS } from '../store/shared.js'
 import NewEventDialog from "../views/NewEventDialog.vue"
+import DeleteDialog from './DeleteDialog.vue'
 
-const eventsColumns = [
+const columns = [
     { name: "date", align: "left", label: "Date", field: "date", sortable: true },
     { name: "time", align: "left", label: "Heure", field: "", sortable: true },
     { name: "type", align: "left", label: "Type", field: "eventType", sortable: true },
@@ -40,9 +65,9 @@ const eventsColumns = [
 
 export default {
     name: 'EventsTable',
-    components: { NewEventDialog },
-    props: { 'events': Object, 'edit': Boolean, 'modelValue': Object },
-    emits: [],
+    components: { NewEventDialog, DeleteDialog },
+    props: { 'edit': Boolean, 'modelValue': Object }, //  'events': Object,
+    emits: ['update:modelValue'],
     setup() {
         return {
         }
@@ -50,20 +75,39 @@ export default {
     data() {
         return {
             store,
-            dialog: { newEvent: false },
-            eventsColumns: eventsColumns,
+            selected: null,
+            dialog: { newEvent: false, deletion: false },
+            columns: columns,
         }
     },
     computed: {
+        events: {
+            get() {
+                return this.modelValue
+            },
+            set(events) {
+                this.$emit('update:modelValue', events)
+            }
+        }
     },
     created() {
     },
     mounted() {
     },
     methods: {
+        downloadICS,
         addEvent() {
-            // this.dialog.newEvent = true
+            this.dialog.newEvent = true
         },
+        handleDeletion(val) {
+            this.selected = val
+            this.dialog.deletion = true
+        },
+        async remove() {
+            // store.events = store.events.filter((x) => (x.id !== this.selected))
+            // this.rows = store.events
+            this.events = this.events.filter((x) => (x.id !== this.selected))
+        }
     }
 }
 </script>
