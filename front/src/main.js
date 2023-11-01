cf8cd01079e069013694320c13d2dd289332e90e
Matthew Parkan <matthew.parkan@gmail.com>
2023-11-01 10:47:11 +0100

    Moved routing code to router.js

--- front/src/main.js
+++ front/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import { Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/fr'
 import App from './App.vue'
-import { store } from './store/store.js'
+import { router } from './router.js'
 
+/*
 import ItemsList from './views/ItemsList.vue'
 import EntitiesList from './views/EntitiesList.vue'
 import EventsList from './views/EventsList.vue'
@@ -15,65 +15,12 @@ import Item from './views/Item.vue'
 import NewItem from './views/NewItem.vue'
 import Entity from './views/Entity.vue'
 import NewEntity from './views/NewEntity.vue'
-// import UsersList from './views/UsersList.vue'
 import User from './views/User.vue'
 import NewUser from './views/NewUser.vue'
 import Login from './views/Login.vue'
 import Admin from './views/Admin.vue'
 import Help from './views/Help.vue'
-
-const routes = [
-    { path: '/', redirect: '/items' },
-    { path: '/items', name: 'ItemsList', component: ItemsList },
-    { path: '/items/:id', name: 'Item', component: Item, props: true },
-    { path: '/items/new', name: 'NewItem', component: NewItem },
-    { path: '/entities', name: 'EntitiesList', component: EntitiesList },
-    { path: '/entities/:id', name: 'Entity', component: Entity, props: true },
-    { path: '/entities/new', name: 'NewEntity', component: NewEntity },
-    { path: '/events', name: 'EventsList', component: EventsList },
-    { path: '/events/:id', name: 'Event', component: Event, props: true },
-    { path: '/events/new', name: 'NewEvent', component: NewEvent },
-    { path: '/statistics', name: 'Statistics', component: Statistics },
-    /* { path: '/users', name: 'UsersList', component: UsersList }, */
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/admin', name: 'Admin', component: Admin },
-    { path: '/admin/users/:id', name: 'User', component: User, props: true },
-    { path: '/admin/users/new', name: 'NewUser', component: NewUser },
-    { path: '/help', name: 'Help', component: Help }
-]
-
-const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: createWebHashHistory(),
-    routes, // short for `routes: routes`
-})
-
-// navigation guards
-router.beforeEach((to, from) => {
-
-    store.navigation.from = from.fullPath
-    store.navigation.to = to.fullPath
-    // ...
-    // explicitly return false to cancel the navigation
-    console.log(`from: ${from}, to: ${to}`)
-    if (store.warning) {
-        // alert('Attention - Modifications non enregistrées')
-
-
-        //store.warning = true
-        //console.log(`store.warning: ${store.warning}`)
-        console.log(from)
-        console.log(to)
-
-        store.exit = true
-        return false
-
-
-    } else {
-        return true
-    }
-
-})
+*/
 
 const app = createApp(App)
 app.use(Quasar, {
