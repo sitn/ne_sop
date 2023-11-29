1199ecb61eb649f32216b52c84e2a76f2d2b0d57
Matthew Parkan <matthew.parkan@gmail.com>
2023-11-29 17:38:44 +0100

    Added temporary test route (entitylist) using django generic views

--- back/ne_sop_backend/ne_sop_api/urls.py
+++ back/ne_sop_backend/ne_sop_api/urls.py
@@ -8,7 +8,9 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r"item", views.ItemViewSet, basename="item")
 router.register(r"item-type", views.ItemTypeViewSet, basename="item-type")
+router.register(r"item-status", views.ItemStatusViewSet, basename="item-status")
 router.register(r"entity", views.EntityViewSet, basename="entity")
+# router.register(r"entity-list", views.EntityListViewSet, basename="entity-list")
 router.register(r"entity-type", views.EntityTypeViewSet, basename="entity-type")
 router.register(r"event", views.EventViewSet, basename="event")
 router.register(r"event-type", views.EventTypeViewSet, basename="event-type")
@@ -25,6 +27,11 @@ urlpatterns = [
         name="entity-uuid",
     ),
     path(
+        "api/entitylist/",
+        views.EntityListViewSet.as_view(),
+        name="entitylist",
+    ),
+    path(
         "api/schema/",
         SpectacularAPIView.as_view(),
         name="schema",
@@ -35,6 +42,14 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
+
+
+"""     path(
+        "api/entitylist/",
+        views.EntityListViewSet.as_view(),
+        name="entity-list",
+    ), """
+
 
 # https://github.com/encode/django-rest-framework/issues/1337
 # urlpatterns = format_suffix_patterns(urlpatterns)
