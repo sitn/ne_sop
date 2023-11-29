2bfb6868dfcfa66e2292defc27b02d87aa6f0f6a
Matthew Parkan <matthew.parkan@gmail.com>
2023-11-29 17:40:44 +0100

    Update Entity.vue

--- front/src/views/Entity.vue
+++ front/src/views/Entity.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="" v-if="loaded">
+    <div class="" v-if="!store.loading">
         <q-layout>
 
             <!-- BREADCRUMBS NAVIGATION -->
             <div class="q-pa-sm q-gutter-sm">
                 <q-breadcrumbs style="font-size: 16px">
                     <q-breadcrumbs-el label="Personnes et groupes" to="/entities" />
-                    <q-breadcrumbs-el :label="entity.name" />
+                    <q-breadcrumbs-el :label="store.entity.name" />
                 </q-breadcrumbs>
             </div>
 
             <!-- FORM -->
-            <EntityForm v-model="entity" :edit="edit"></EntityForm>
+            <EntityForm v-model="store.entity" :edit="edit"></EntityForm>
 
             <!-- FLOATING ACTION BUTTONS -->
             <FloatingButtons :edit="edit" :wait="wait" :buttons="actionButtons" @save-event="save" @delete-event="handleDeletion" @edit-event="setEditMode"></FloatingButtons>
@@ -52,7 +52,7 @@ export default {
             // actionButtons: { save: 'active', deletion: 'active' },
             edit: false,
             wait: false,
-            entity: null, // store.entities.find(e => e.id === this.$route.params.id),
+            entity: store.entity, // store.entities.find(e => e.id === this.$route.params.id),
             index: store.entities.findIndex((e) => (e.id === this.$route.params.id))
         }
     },
@@ -60,19 +60,18 @@ export default {
 
         actionButtons() {
             return {
-                save: this.entity.valid ? 'active' : 'disable',
+                save: store.entity.valid ? 'active' : 'disable', // this.entity.valid ? 'active' : 'disable',
                 deletion: 'none'
             }
         }
 
     },
     beforeCreate() {
-
+        store.getEntity(this.$route.params.id)
     },
     async created() {
 
-        
-
+        /*
         try {
 
             const id = this.$route.params.id
@@ -82,13 +81,19 @@ export default {
             })
             this.entity = await response.json()
             this.loaded = true
+            store.loading = false
             console.log(this.entity)
 
         } catch (error) {
             console.error(error)
         }
+        */
 
+        //this.loaded = !store.loading
         // store.getEntity(this.$route.params.id)
+
+        // store.loaded = true
+
         // store.saveButton = true
         // this.entity = Object.assign({}, store.entities.find((e) => (e.id === this.$route.params.id)))
         //store.getEntity(this.$route.params.id)
@@ -97,8 +102,9 @@ export default {
         // this.load(this.$route.params.id)
 
     },
-    mounted() {
-        //store.getEntity(this.$route.params.id)
+    async mounted() {
+        // store.getEntity(this.$route.params.id)
+        // await store.loading
         //this.entity = store.entity
 
     },
@@ -120,12 +126,13 @@ export default {
             }
         },
         async save(redirectTo) {
+
             // TODO: POST RECORD TO DATABASE
             console.log(`${this.$options.name}.vue | save()`)
             this.wait = true
-            await sleep(Math.random() * 1300)
-            // let ind = store.entities.findIndex((e) => (e.id === this.$route.params.id))
-            store.entities[this.index] = Object.assign({}, this.entity)
+            // await sleep(Math.random() * 1300)
+            // store.entities[this.index] = Object.assign({}, this.entity)
+            this.store.updateEntity(this.$route.params.id, store.entity)
             this.wait = false
 
             if (redirectTo !== null) {
