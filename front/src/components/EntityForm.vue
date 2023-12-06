e06f5433724c4d20f60b48acc9cd2a856fe62392
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-06 16:04:15 +0100

    Update EntityForm.vue
    Use async call to API and load data in component (instead of JS store)

--- front/src/components/EntityForm.vue
+++ front/src/components/EntityForm.vue
@@ -19,7 +19,7 @@
                             <!-- TYPE SELECT FIELD -->
                             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 
-                                <q-select bg-color="white" outlined v-model="entity.type" :options="store.entityTypes" option-label="name" option-value="id" emit-value map-options label="Type" clearable :rules="[v => checkFilled(v)]" :disable="!edit">
+                                <q-select bg-color="white" outlined v-model="entity.type" :options="entityTypes" option-label="name" option-value="id" emit-value map-options label="Type" clearable :rules="[v => checkFilled(v)]" :disable="!edit">
                                     <template v-slot:option="scope">
                                         <q-item v-bind="scope.itemProps">
                                             <q-item-section>
@@ -145,7 +145,7 @@ export default {
     data() {
         return {
             store,
-            entityTypes: null,
+            entityTypes: [],
         }
     },
     computed: {
@@ -158,10 +158,11 @@ export default {
             }
         }
     },
-    beforeCreate() {
-        this.store.getEntityTypes()
+    async beforeCreate() {
+
     },
-    created() {
+    async created() {
+        this.entityTypes = await this.store.getEntityTypes()
         console.log(`${this.$options.name} | router id: ${this.$route.params.id}`)
     },
     mounted() {
