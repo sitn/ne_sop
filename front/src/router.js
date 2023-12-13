daa94bd46dce67ce0003bdbcf2fc98ab6b47097a
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-13 16:37:31 +0100

    Added new routes (still testing)

--- front/src/router.js
+++ front/src/router.js
@@ -5,12 +5,15 @@ import ItemsList from './views/ItemsList.vue'
 import EntitiesList from './views/EntitiesList.vue'
 import EventsList from './views/EventsList.vue'
 import Event from './views/Event.vue'
+import EventUniversal from './views/EventUniversal.vue'
 import NewEvent from './views/NewEvent.vue'
 import Statistics from './views/Statistics.vue'
 import Item from './views/Item.vue'
 import NewItem from './views/NewItem.vue'
+import ItemUniversal from './views/ItemUniversal.vue'
 import Entity from './views/Entity.vue'
 import NewEntity from './views/NewEntity.vue'
+import EntityUniversal from './views/EntityUniversal.vue'
 import User from './views/User.vue'
 import NewUser from './views/NewUser.vue'
 import Login from './views/Login.vue'
@@ -20,14 +23,26 @@ import Help from './views/Help.vue'
 const routes = [
     { path: '/', redirect: '/items' },
     { path: '/items', name: 'ItemsList', component: ItemsList },
+    /*
     { path: '/items/:id', name: 'Item', component: Item, props: true },
     { path: '/items/new', name: 'NewItem', component: NewItem },
+    */
+    { path: '/items/:id', name: 'Item', component: ItemUniversal, props: true },
+    { path: '/items/new', name: 'NewItem', component: ItemUniversal },
     { path: '/entities', name: 'EntitiesList', component: EntitiesList },
+    /*
     { path: '/entities/:id', name: 'Entity', component: Entity, props: true },
     { path: '/entities/new', name: 'NewEntity', component: NewEntity },
+    */
+    { path: '/entities/:id', name: 'Entity', component: EntityUniversal, props: true },
+    { path: '/entities/new', name: 'NewEntity', component: EntityUniversal },
     { path: '/events', name: 'EventsList', component: EventsList },
+    /*
     { path: '/events/:id', name: 'Event', component: Event, props: true },
     { path: '/events/new', name: 'NewEvent', component: NewEvent },
+    */
+    { path: '/events/:id', name: 'Event', component: EventUniversal, props: true },
+    { path: '/events/new', name: 'NewEvent', component: EventUniversal },
     { path: '/statistics', name: 'Statistics', component: Statistics },
     { path: '/login', name: 'Login', component: Login },
     { path: '/admin', name: 'Admin', component: Admin },
