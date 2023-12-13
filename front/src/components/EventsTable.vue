09ba3468359bb6f8fa980c4db00f8e264c964f72
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-13 16:40:11 +0100

    Update EventsTable.vue

--- front/src/components/EventsTable.vue
+++ front/src/components/EventsTable.vue
@@ -11,7 +11,7 @@
     </div>
 
     <!-- EVENTS TABLE -->
-    <q-table :rows="events" :columns="columns" row-key="date" @request="onRequest" binary-state-sort class="q-my-md">
+    <q-table :rows="events" :columns="columns" row-key="date" @request="onRequest" binary-state-sort class="q-my-md" v-if="eventTypes.length > 0"> <!-- v-if="this.eventTypes.length > 0" -->
         <template v-slot:body="props">
             <q-tr :props="props">
                 <q-td key="date" :props="props">
@@ -23,10 +23,16 @@
                 <q-td key="type" :props="props">
                     {{ getEventTypeName(props.row.type) }}
                 </q-td>
+                <q-td key="description" :props="props">
+                    {{ props.row.description }}
+                </q-td>
                 <q-td key="actions" :props="props">
                     <div class="float-right">
                         <q-btn dense round flat color="grey" name="calendar" @click="downloadICS(props.row)" icon="sym_o_calendar_add_on" :disable="!edit">
                             <q-tooltip class="bg-black">Ajouter au calendrier</q-tooltip>
+                        </q-btn>
+                        <q-btn dense round flat color="blue" name="delete" @click="handleEdition(props.row)" icon="sym_o_edit" :disable="!edit">
+                            <q-tooltip class="bg-black">Modifier</q-tooltip>
                         </q-btn>
                         <q-btn dense round flat color="red" name="delete" @click="handleDeletion(props.row.id)" icon="sym_o_delete" :disable="!edit">
                             <q-tooltip class="bg-black">Supprimer</q-tooltip>
@@ -46,6 +52,11 @@
         <NewEventDialog v-model="events"></NewEventDialog>
     </q-dialog>
 
+    <!-- EDIT EVENT DIALOG -->
+    <q-dialog v-model="dialog.edition">
+        <EditEventDialog v-model="selected"></EditEventDialog>
+    </q-dialog>
+
     <!-- DELETE EVENT DIALOG -->
     <DeleteDialog v-model="dialog.deletion" @delete-event="remove" />
 </template>
@@ -54,18 +65,20 @@
 import { store } from '../store/store.js'
 import { downloadICS } from '../store/shared.js'
 import NewEventDialog from "../views/NewEventDialog.vue"
+import EditEventDialog from "../views/EditEventDialog.vue"
 import DeleteDialog from './DeleteDialog.vue'
 
 const columns = [
     { name: "date", align: "left", label: "Date", field: "date", sortable: true },
     { name: "time", align: "left", label: "Heure", field: "", sortable: true },
     { name: "type", align: "left", label: "Type", field: "eventType", sortable: true },
+    { name: "description", align: "left", label: "Note", field: "description", sortable: false },
     { name: "actions", align: "right", label: "", field: "", sortable: false }
 ]
 
 export default {
     name: 'EventsTable',
-    components: { NewEventDialog, DeleteDialog },
+    components: { NewEventDialog, EditEventDialog, DeleteDialog },
     props: { 'item': Number, 'edit': Boolean, 'modelValue': Object, 'mode': String }, //  'events': Object,
     emits: ['update:modelValue'],
     setup() {
@@ -76,12 +89,13 @@ export default {
         return {
             store,
             selected: null,
-            dialog: { newEvent: false, deletion: false },
+            dialog: { newEvent: false, deletion: false, edition: false },
             eventTypes: [],
             data: null,
             rows: [],
             columns: columns,
-            loading: false,
+            loading: true,
+            render: false,
             pagination: {
                 sortBy: "date",
                 descending: false,
@@ -105,18 +119,9 @@ export default {
         console.log(`${this.$options.name} | mode: ${this.mode}`)
 
         this.eventTypes = await this.store.getEventTypes()
+        this.render = true
 
-        console.log('this.eventTypes')
-        console.log(this.eventTypes)
-        console.log(this.getEventTypeName(1))
-
-
-        if (this.mode === "create") {
-
-
-
-        }
-
+        /*
         if (this.mode === "update") {
             this.loading = true
             this.data = await store.getEvents("", this.item, this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
@@ -124,9 +129,26 @@ export default {
             this.pagination.rowsNumber = this.data.nrows
             this.loading = false
         }
+        */
+
+
+        if (this.mode === "create") {
+
+            this.loading = true
+            this.data = await store.getEvents("", this.item, this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
+            this.rows = this.data.results
+            this.pagination.rowsNumber = this.data.nrows
+            this.loading = false
+
+            console.log('this.eventTypes')
+            console.log(this.eventTypes)
+            console.log(this.getEventTypeName(1))
+
+        }
 
     },
     mounted() {
+
     },
     methods: {
         downloadICS,
@@ -170,6 +192,12 @@ export default {
             this.selected = val
             this.dialog.deletion = true
         },
+        handleEdition(val) {
+            this.selected = val
+            console.log('handleEdition')
+            console.log(this.selected)
+            this.dialog.edition = true
+        },
         async remove() {
 
             // TODO: REPLACE WITH GET CALL TO API
@@ -180,8 +208,11 @@ export default {
                 this.events = this.events.filter((x) => (x.id !== this.selected))
             }
 
-            // Delete event in local JSON
-            // this.events = this.events.filter((x) => (x.id !== this.selected))
+            if (this.mode === "update") {
+
+                // Delete event in local JSON
+                this.events = this.events.filter((x) => (x.id !== this.selected))
+            }
 
             /*
             // Delete event in DB
@@ -198,7 +229,7 @@ export default {
 
         },
         getEventTypeName(id) {
-            return this.eventTypes.find((x) => x.id == id).name
+            return this.eventTypes.find((x) => x.id === id).name
         }
     }
 }
