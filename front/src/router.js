3aee38e85e9d070b2d783d594074251abb6e71ed
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-14 12:03:16 +0100

    Removed unused routes and comment cleanup

--- front/src/router.js
+++ front/src/router.js
@@ -5,15 +5,9 @@ import ItemsList from './views/ItemsList.vue'
 import EntitiesList from './views/EntitiesList.vue'
 import EventsList from './views/EventsList.vue'
 import Event from './views/Event.vue'
-import EventUniversal from './views/EventUniversal.vue'
-import NewEvent from './views/NewEvent.vue'
 import Statistics from './views/Statistics.vue'
 import Item from './views/Item.vue'
-import NewItem from './views/NewItem.vue'
-import ItemUniversal from './views/ItemUniversal.vue'
 import Entity from './views/Entity.vue'
-import NewEntity from './views/NewEntity.vue'
-import EntityUniversal from './views/EntityUniversal.vue'
 import User from './views/User.vue'
 import NewUser from './views/NewUser.vue'
 import Login from './views/Login.vue'
@@ -23,26 +17,14 @@ import Help from './views/Help.vue'
 const routes = [
     { path: '/', redirect: '/items' },
     { path: '/items', name: 'ItemsList', component: ItemsList },
-    /*
     { path: '/items/:id', name: 'Item', component: Item, props: true },
-    { path: '/items/new', name: 'NewItem', component: NewItem },
-    */
-    { path: '/items/:id', name: 'Item', component: ItemUniversal, props: true },
-    { path: '/items/new', name: 'NewItem', component: ItemUniversal },
+    { path: '/items/new', name: 'NewItem', component: Item },
     { path: '/entities', name: 'EntitiesList', component: EntitiesList },
-    /*
     { path: '/entities/:id', name: 'Entity', component: Entity, props: true },
-    { path: '/entities/new', name: 'NewEntity', component: NewEntity },
-    */
-    { path: '/entities/:id', name: 'Entity', component: EntityUniversal, props: true },
-    { path: '/entities/new', name: 'NewEntity', component: EntityUniversal },
+    { path: '/entities/new', name: 'NewEntity', component: Entity },
     { path: '/events', name: 'EventsList', component: EventsList },
-    /*
     { path: '/events/:id', name: 'Event', component: Event, props: true },
-    { path: '/events/new', name: 'NewEvent', component: NewEvent },
-    */
-    { path: '/events/:id', name: 'Event', component: EventUniversal, props: true },
-    { path: '/events/new', name: 'NewEvent', component: EventUniversal },
+    { path: '/events/new', name: 'NewEvent', component: Event },
     { path: '/statistics', name: 'Statistics', component: Statistics },
     { path: '/login', name: 'Login', component: Login },
     { path: '/admin', name: 'Admin', component: Admin },
@@ -52,9 +34,8 @@ const routes = [
 ]
 
 export const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
-    routes // short for `routes: routes`
+    routes
 })
 
 // navigation guards
