d971ae8d93d9a67d77cd7cccc1fb316c665a447e
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-14 12:04:14 +0100

    Removed unused code

--- front/src/views/Event.vue
+++ front/src/views/Event.vue
@@ -8,18 +8,11 @@
                 <q-breadcrumbs style="font-size: 16px">
                     <q-breadcrumbs-el label="Calendrier" to="/events" />
                     <q-breadcrumbs-el :label="event.date" />
-                    <!--  <q-breadcrumbs-el :label="store.event.date" />-->
                 </q-breadcrumbs>
             </div>
 
-            <!--  <div>valid: {{ valid }}</div> -->
-            <!--  <div>store.valid: {{ store.valid }}</div> -->
-
             <!-- FORM -->
             <EventForm v-model="event" :edit="edit"></EventForm>
-            <!-- <EventForm v-model="store.event" :edit="edit" @validation-event="handleValidation"></EventForm> -->
-            <!-- <EventForm v-model="event" :edit="edit" @validation-event="handleValidation"></EventForm> -->
-            <!-- <EventForm v-model="event" :edit="edit" @validation-event="handleValidation" ref="EventForm"></EventForm> -->
 
             <!-- FLOATING ACTION BUTTONS -->
             <FloatingButtons :edit="edit" :wait="wait" :buttons="actionButtons" @save-event="save" @delete-event="handleDeletion" @edit-event="setEditMode"></FloatingButtons>
@@ -33,8 +26,6 @@
 
 <script>
 import { store } from '../store/store.js'
-// import { sleep } from '../store/shared.js'
-// import eventTypes from '../assets/data/event-types.json'
 import FloatingButtons from "../components/FloatingButtons.vue"
 import DeleteDialog from '../components/DeleteDialog.vue'
 import EventForm from "../components/EventForm.vue"
@@ -44,107 +35,68 @@ export default {
     components: { FloatingButtons, DeleteDialog, EventForm },
     props: {},
     emits: [],
-    setup() {
-        return {
-        }
-    },
     data() {
         return {
             store,
             dialog: { deletion: false },
-            // actionButtons: { save: 'active', deletion: 'active' },
+            loading: true,
             edit: false,
             wait: false,
             valid: null,
-            event: null, // store.event, // store.events.find(e => e.id === this.$route.params.id),
-            index: store.events.findIndex((e) => (e.id === this.$route.params.id))
+            event: {
+                "date": "",
+                "time": "",
+                "item": null,
+                "type": "",
+                "description": "",
+                "valid": false
+            },
         }
     },
     computed: {
         actionButtons() {
             return {
-                save: this.event.valid ? 'active' : 'disable', //   store.event.valid ? 'active' : 'disable'
+                save: this.event.valid ? 'active' : 'disable',
                 deletion: 'none'
             }
         }
     },
-    beforeCreate() {
-    },
     async created() {
 
-        this.store.loading = true
-        this.event = await store.getEvent(this.$route.params.id)
+        // console.log(`this.$route.params.id: ${this.$route.params.id}`)
+        if (this.$route.params.hasOwnProperty('id')) {
+            this.store.loading = true
+            this.event = await store.getEvent(this.$route.params.id)
+            this.store.loading = false
+        }
         this.store.loading = false
 
-        // store.getEvent(this.$route.params.id)
-        // this.event = Object.assign({}, store.events.find(e => e.id === this.$route.params.id))
     },
-    mounted() {
-        // TODO: store.valid ? this.actionButtons.save = 'active' : this.actionButtons.save = 'disable'
-    },
-
     methods: {
-        /*
-        validate() {
-            console.log('this.$refs.EventForm.validateForm()')
-            this.$refs.EventForm.validateForm()
-        },
-        */
-        async save(redirectTo) {
+        async save() {
 
-            // TODO: POST RECORD TO DATABASE
-            let message = await store.updateEvent(this.$route.params.id, this.event)
-            if (message) {
-                this.wait = false
-                console.log(message)
-
-                if (redirectTo !== null) {
-                    this.$router.push({ path: redirectTo })
-                }
-            }
-
-            /*
-            console.log(`${this.$options.name} | save()`)
             this.wait = true
-            await sleep(Math.random() * 1300)
-            store.items.forEach((x, j) => {
-                let k = x.events.findIndex(y => (y.id === this.event.id))
-                if (k !== -1) {
-                    store.items[j].events[k] = Object.assign({}, this.event)
-                }
-            })
-            store.events[this.index] = Object.assign({}, this.event)
-
-            this.wait = false
-
-            if (redirectTo !== null) {
-                this.$router.push({ path: redirectTo })
+            if (this.event.id) {
+                this.event = await store.updateEvent(this.event.id, this.event)
+            } else {
+                this.event = await store.addEvent(this.event)
             }
-            */
+            this.wait = false
 
         },
         handleValidation(val) {
-
             this.valid = val
-            //this.valid ? this.actionButtons.save = 'active' : this.actionButtons.save = 'disable'
-
-            // store.valid ? this.actionButtons.save = 'active' : this.actionButtons.save = 'disable'
-            console.log(`${this.$options.name} | handleValidation()`)
-            // this.actionButtons.save = 'disable'
         },
         handleDeletion() {
             this.dialog.deletion = true
         },
         async remove() {
             // TODO: DELETE RECORD IN DATABASE
-            console.log(`${this.$options.name} | remove()`)
-            store.events.splice(this.index, 1)
             this.$router.push({ name: 'EventsList' })
         },
         setEditMode(val) {
-            console.log(`${this.$options.name} | setEditMode(${val})`)
+            // console.log(`${this.$options.name} | setEditMode(${val})`)
             this.edit = val
-            // this.$refs.EventForm.validateForm()
         }
     }
 }
