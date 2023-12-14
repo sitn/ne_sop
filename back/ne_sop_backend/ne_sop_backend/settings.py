5baa9d2da028581da7085809b58a37707c2a257b
maltaesousa <stephane.maltaesousa@ne.ch>
2023-12-14 16:56:54 +0100

    revert django middleware

--- back/ne_sop_backend/ne_sop_backend/settings.py
+++ back/ne_sop_backend/ne_sop_backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "ne_sop_backend.middleware.RemoteUserMiddleware",
+    "django.contrib.auth.middleware.RemoteUserMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # CORS
