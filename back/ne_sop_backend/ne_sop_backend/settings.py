297737721db84aadff15613156e20be1aabc940f
maltaesousa <stephane.maltaesousa@ne.ch>
2023-12-18 15:31:21 +0100

    Fix static path

--- back/ne_sop_backend/ne_sop_backend/settings.py
+++ back/ne_sop_backend/ne_sop_backend/settings.py
@@ -28,7 +28,7 @@ load_dotenv(PurePath(Path(BASE_DIR).resolve().parent, ".env"))
 SECRET_KEY = os.environ["NESOP_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True if os.environ.get('DEBUG', '') == 'True' else False
 
 ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
@@ -143,7 +143,7 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "ne_sop_backend", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
