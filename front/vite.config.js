a5d581ba9030fd01c190333857760df3b9919d20
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-27 14:29:11 +0100

    Added import of environnement variables, commented unused code

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,35 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import { resolve } from 'path'
 
-const projectRootDir = resolve(__dirname)
+// const projectRootDir = resolve(__dirname)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/web/sop/', // /front/
-  publicDir: 'public',
-  plugins: [
-    /* vue(),
-    quasar({
-      lang: 'fr',
-    }),
-    */
-    vue({
-      template: { transformAssetUrls }
-    }),
-    quasar({
-      config: {
-        lang: 'fr'
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+
+    // export default defineConfig({
+    base: env.VITE_BASE_PATH, //'/web/sop/', // /front/
+    publicDir: 'public',
+    plugins: [
+      vue({
+        template: { transformAssetUrls }
+      }),
+      quasar({
+        config: {
+          lang: 'fr'
+        }
+      })
+    ],
+    /*
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@assets/*': fileURLToPath(new URL('./src/assets/', import.meta.url)),
+        '@public/*': fileURLToPath(new URL('./public/', import.meta.url)),
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets/*': fileURLToPath(new URL('./src/assets/', import.meta.url)),
-      '@public/*': fileURLToPath(new URL('./public/', import.meta.url)),
     }
+    */
   }
 })
