6a55d16209e701ab4fedd97399a1aa7709a472c6
Matthew Parkan <matthew.parkan@gmail.com>
2023-12-28 17:39:12 +0100

    Minor formatting

--- back/ne_sop_backend/ne_sop_api/permissions.py
+++ back/ne_sop_backend/ne_sop_api/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 from ne_sop_api.models import Item
 
+
 class IsManagerPermission(permissions.BasePermission):
     """
     This will be reimplemented when OpenID will be fully functionnal.
@@ -8,10 +9,11 @@ class IsManagerPermission(permissions.BasePermission):
     This permission is given to one or many users that are allowed to
     CRUD everything in app because they belong to Manager group.
     """
-    message = 'Current user is not in Manager group.'
+
+    message = "Current user is not in Manager group."
 
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Manager').exists()
+        return request.user.groups.filter(name="Manager").exists()
 
 
 class IsManagerOrReadOnlyPermission(permissions.BasePermission):
@@ -22,12 +24,13 @@ class IsManagerOrReadOnlyPermission(permissions.BasePermission):
     CRUD everything in app because they belong to Manager group.
     If they don't belong to Manager group, they can only view
     """
-    message = 'Current user is not in Manager group.'
+
+    message = "Current user is not in Manager group."
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.groups.filter(name='Manager').exists()
+        return request.user.groups.filter(name="Manager").exists()
 
 
 class IsManagerOrReadUpdatePermission(permissions.BasePermission):
@@ -37,12 +40,14 @@ class IsManagerOrReadUpdatePermission(permissions.BasePermission):
     This permission allow everything but POST and DELETE. Only Manager group is
     allowed to POST
     """
-    message = 'Current user is not in Manager group.'
+
+    message = "Current user is not in Manager group."
 
     def has_object_permission(self, request, view, obj):
-        if request.method in ['POST', 'DELETE']:
-            return request.user.groups.filter(name='Manager').exists()
+        if request.method in ["POST", "DELETE"]:
+            return request.user.groups.filter(name="Manager").exists()
         return True
+
 
 def isAllowedRegadingEntitiesAndItemId(user, item_id):
     Item.objects.get(item_id).support.values_list("id", flat=True)
