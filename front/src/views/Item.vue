d7d37bcfb38584caadef9f48544463b569f5cad2
Matthew Parkan <matthew.parkan@gmail.com>
2024-02-01 14:40:42 +0100

    Added "autonotify" attribute

--- front/src/views/Item.vue
+++ front/src/views/Item.vue
@@ -61,6 +61,7 @@ export default {
                 "urgent": false,
                 "writtenresponse": false,
                 "oralresponse": false,
+                "autonotify": false,
                 /* "fight": false, */
                 "author": null,
                 "lead": null,
