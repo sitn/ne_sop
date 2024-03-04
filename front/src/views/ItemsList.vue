53e0fe873c00a90003b74811c8a4865382bd0389
Matthew Parkan <matthew.parkan@gmail.com>
2024-03-04 12:03:46 +0100

    Update ItemsList.vue
    - Added filter and clear buttons
    - Code cleanup

--- front/src/views/ItemsList.vue
+++ front/src/views/ItemsList.vue
@@ -10,17 +10,22 @@
 
             <!-- SEARCH ITEMS FIELD -->
             <div class="col-xs-12 col-sm-8 col-md-6 col-lg-6">
-                <q-input bg-color="white" v-model="searchString" outlined dense placeholder="Rechercher" @update:model-value="query()">
+
+                <q-input class="q-pa-none q-ma-none" bg-color="white" v-model="filter.search" outlined dense placeholder="Rechercher"> <!-- @update:model-value="query()"-->
                     <template v-slot:prepend>
                         <q-icon name="sym_o_search" />
                     </template>
+
                     <template v-slot:append>
                         <q-spinner color="blue-grey" :thickness="3" v-if="loading" />
-                        <!-- 
-                            <q-btn unelevated icon="sym_o_filter_alt" padding="xs" @click="console.log('filter')">
-                                <q-tooltip class="bg-black">Filtrer</q-tooltip>
-                            </q-btn>
-                            -->
+
+                        <q-btn unelevated no-caps icon="sym_o_filter_alt" dense padding="xs" label="Filtres" @click="handleFilter()">
+                            <q-tooltip class="bg-black">Filtrer</q-tooltip>
+                        </q-btn>
+
+                        <q-btn unelevated dense icon="close" @click="reset">
+                            <q-tooltip class="bg-black">Réinitialiser</q-tooltip>
+                        </q-btn>
                     </template>
 
                 </q-input>
@@ -28,6 +33,7 @@
 
             <!-- ADD NEW ITEM BUTTON -->
             <div class="col-xs-12 col-sm-4 col-md-6 col-lg-6" v-if="store.user.is_manager">
+
                 <q-btn padding="sm md" unelevated no-caps color="blue-grey-8" text-color="white" icon="sym_o_add_circle" label="Ajouter" class="q-py-none q-my-none" to="/items/new">
                     <q-tooltip class="bg-black">Ajouter un nouvel objet parlementaire</q-tooltip>
                 </q-btn>
@@ -35,83 +41,91 @@
 
         </div>
 
-           <!-- ITEMS TABLE -->
-           <q-table title="" :rows="rows" :columns="columns" row-key="id" v-model:pagination="pagination" :loading="loading" @request="onRequest" binary-state-sort class="q-my-lg"> <!-- :filter="filter" -->
+        <!-- ITEMS TABLE -->
+        <q-table title="" :rows="rows" :columns="columns" row-key="id" v-model:pagination="pagination" :loading="loading" @request="onRequest" binary-state-sort class="q-my-lg"> <!-- :filter="filter" -->
 
-        <!-- TABLE BODY -->
-        <template v-slot:body="props">
-            <q-tr :props="props">
+            <!-- TABLE BODY -->
+            <template v-slot:body="props">
+                <q-tr :props="props">
 
-                <!-- STATUS COLUMN -->
-                <q-td key="status" :props="props">
+                    <!-- STATUS COLUMN -->
+                    <q-td key="status" :props="props">
 
-                    <!-- 
+                        <!-- 
                     <q-avatar rounded size="58px" v-if="props.row.islate">
                         <img src="img/kermit-panicking.gif">
                     </q-avatar>
                     -->
 
-                    <!-- <q-badge floating color="yellow" text-color="black">retard</q-badge> -->
+                        <!-- <q-badge floating color="yellow" text-color="black">retard</q-badge> -->
 
 
-                    <div class="q-gutter-xs">
-                        <q-badge color="red" class="q-my-sm" v-if="props.row.urgent">Urgent</q-badge>
+                        <div class="q-gutter-xs">
+                            <q-badge color="red" class="q-my-sm" v-if="props.row.urgent">Urgent</q-badge>
+                            <q-badge color="yellow" text-color="black" class="q-my-sm blink" v-if="props.row.late"> Retard</q-badge>
+                        </div>
+                        <div class="row items-center">
+                            <q-badge :color="props.row.status.color" rounded class="q-mr-sm" />
+                            <div>{{ props.row.status.name }}</div>
+                        </div>
 
-                        <q-badge color="yellow" text-color="black" class="q-my-sm blink" v-if="props.row.late"> Retard</q-badge>
-                    </div>
-                    <div class="row items-center">
-                        <q-badge :color="props.row.status.color" rounded class="q-mr-sm" />
-                        <div>{{ props.row.status.name }}</div>
-                    </div>
+                    </q-td>
 
-                </q-td>
+                    <!-- NUMBER COLUMN -->
+                    <q-td key="number" :props="props">
+                        {{ props.row.number }}
+                    </q-td>
+                    <!-- TYPE COLUMN -->
+                    <q-td key="type" :props="props">
+                        {{ props.row.type.name }}
+                    </q-td>
+                    <!-- TITLE COLUMN -->
+                    <q-td key="title" :props="props">
 
-                <!-- NUMBER COLUMN -->
-                <q-td key="number" :props="props">
-                    {{ props.row.number }}
-                </q-td>
-                <!-- TYPE COLUMN -->
-                <q-td key="type" :props="props">
-                    {{ props.row.type.name }}
-                </q-td>
-                <!-- TITLE COLUMN -->
-                <q-td key="title" :props="props">
+                        <router-link :to="{
+                            name: 'Item',
+                            params: {
+                                id: props.row.id
+                            }
+                        }">
+                            {{ props.row.title }}
+                        </router-link>
 
-                    <router-link :to="{
-                        name: 'Item',
-                        params: {
-                            id: props.row.id
-                        }
-                    }">
-                        {{ props.row.title }}
-                    </router-link>
-
-                </q-td>
-                <!-- DEPOSIT DATE COLUMN -->
-                <q-td key="deposit" :props="props">
-                    {{ props.row.startdate }}
-                </q-td>
-                <!-- DELAY DATE COLUMN -->
-                <q-td key="delay" :props="props">
-                    {{ props.row.enddate }}
-                </q-td>
-                <!-- ACTIONS COLUMN -->
-                <q-td key="actions" :props="props">
-                    <div class="float-right">
-                        <q-btn dense round flat color="red" name="delete" @click="handleDeletion(props.row.id)" icon="sym_o_delete" v-if="store.user.is_manager">
-                            <q-tooltip class="bg-black">Supprimer</q-tooltip>
-                        </q-btn>
-                    </div>
-                </q-td>
-            </q-tr>
-        </template>
-        <template v-slot:no-data>
-            Aucune objet
-        </template>
+                    </q-td>
+                    <!-- DEPOSIT DATE COLUMN -->
+                    <q-td key="deposit" :props="props">
+                        {{ props.row.startdate }}
+                    </q-td>
+                    <!-- DELAY DATE COLUMN -->
+                    <q-td key="delay" :props="props">
+                        {{ props.row.enddate }}
+                    </q-td>
+                    <!-- ACTIONS COLUMN -->
+                    <q-td key="actions" :props="props">
+                        <div class="float-right">
+                            <q-btn dense round flat color="red" name="delete" @click="handleDeletion(props.row.id)" icon="sym_o_delete" v-if="store.user.is_manager">
+                                <q-tooltip class="bg-black">Supprimer</q-tooltip>
+                            </q-btn>
+                        </div>
+                    </q-td>
+                </q-tr>
+            </template>
+            <template v-slot:no-data>
+                Aucune objet
+            </template>
         </q-table>
 
         <!-- DELETE DIALOG -->
         <DeleteDialog v-model="dialog.deletion" @delete-event="remove" />
+
+        <!-- FILTER DIALOG -->
+        <ItemFilterDialog ref="didi" v-model:show="dialog.filter" v-model:filter="filter"></ItemFilterDialog>
+
+        <!-- 
+        <q-dialog v-model="dialog.filter">
+            <ItemFilterDialog v-model="filter"></ItemFilterDialog>
+        </q-dialog>
+        -->
 
     </div>
 </template>
@@ -119,20 +133,19 @@
 <script>
 import { store } from '../store/store.js'
 import DeleteDialog from '../components/DeleteDialog.vue'
-
+import ItemFilterDialog from './ItemFilterDialog.vue'
 
 export default {
     name: 'ItemsList',
-    components: { DeleteDialog },
+    components: { DeleteDialog, ItemFilterDialog },
     props: { 'title': String, 'model': Object },
     emits: [],
     data() {
         return {
             store,
             selected: null,
-            searchString: "",
-            filter: "",
-            dialog: { deletion: false },
+            filter: { search: "", number: "", title: "", status: [], type: [] },
+            dialog: { deletion: false, filter: false },
             data: null,
             rows: [],
             loading: false,
@@ -196,40 +209,41 @@ export default {
             ],
         }
     },
+    watch: {
+        filter: {
+            handler(newValue, oldValue) {
+                // console.log(`${this.$options.name} | filter()`)
+                this.query()
+            },
+            deep: true
+        },
+    },
     async created() {
 
-        this.loading = true
-        this.data = await store.getItems("", this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
-        this.rows = this.data.results
-        this.pagination.rowsNumber = this.data.nrows
-        this.loading = false
+        // initialize filters
+        this.filter.type = (await store.getItemTypes()).map(x => x.id)
+        this.filter.status = (await store.getItemStatus()).map(x => x.id)
+
+        this.query()
 
     },
     methods: {
         async onRequest(props) {
 
-            this.loading = true
-
-            let { page, rowsPerPage, sortBy, descending } = props.pagination
-            // let filter = props.filter
-            rowsPerPage = rowsPerPage === 0 ? this.pagination.rowsNumber : rowsPerPage
-            this.data = await store.getItems("", page, rowsPerPage, sortBy, descending)
-            this.rows = this.data.results
-
             // update pagination object
             this.pagination = props.pagination
-            this.loading = false
+            this.pagination.rowsPerPage = props.pagination.rowsPerPage === 0 ? this.pagination.rowsNumber : props.pagination.rowsPerPage // rowsPerPage
+
+            // update table rows
+            this.query()
 
         },
         async query() {
 
             this.loading = true
-            if (this.searchString.length >= 3) {
-                this.data = await store.getItems(this.searchString, this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
-            } else {
-                this.data = await store.getItems("", this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
-            }
+            this.data = await store.getItems(this.filter, this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
             this.rows = this.data.results
+            this.pagination.rowsNumber = this.data.nrows
             this.loading = false
 
         },
@@ -239,17 +253,22 @@ export default {
             this.dialog.deletion = true
 
         },
+        handleFilter() {
+
+            this.dialog.filter = true
+
+        },
         async remove() {
 
-            this.loading = true
             // console.log(`delete ${this.selected}`)
             let message = await store.deleteItem(this.selected)
             if (message) {
-                this.data = await store.getItems(this.searchString, this.pagination.page, this.pagination.rowsPerPage, this.pagination.sortBy, this.pagination.descending)
-                this.rows = this.data.results
+                this.query()
             }
-            this.loading = false
 
+        },
+        reset() {
+            this.$refs.didi.resetall()
         },
         getDate(events, type) {
 
