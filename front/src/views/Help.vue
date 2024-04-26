62fccdeb5ee55800bbb0cb2f4c89461de85cffd3
Marc Rufener <marc.rufener@gmail.com>
2024-04-26 16:26:51 +0200

    help - create new person or group

--- front/src/views/Help.vue
+++ front/src/views/Help.vue
@@ -44,6 +44,45 @@
         </template>
     </FormSection>
 
+    <FormSection title="Créer et éditer une personne / Groupe parlementaire" v-if="store.user.is_manager">
+        <template v-slot:content>
+            <br>
+            <p>
+                <div class="row">
+                    <div class="block">
+                        Créer une nouvelle personne ou un nouveau groupe parlementaire
+                        <ol>
+                            <li>Cliquer sur <b>+ Ajouter</b></li>
+                            <li>Renseigner les champs du formulaire</li>
+                            <li>Cliquer sur <b>enregistrer</b> (<span class="text-blue q-gutter-md" style="font-size: 1.5em"><q-icon name="sym_o_save" style="margin-top: 0;"/></span>), puis sur <b>fermer</b> (<span class="text-blue q-gutter-md" style="font-size: 1.5em"><q-icon name="sym_o_close" style="margin-top: 0;"/></span>)</li>
+                        </ol>
+                    </div>
+
+                    <div class="block">
+                        Modifier une personne ou un groupe parlementaire
+                        <ol>
+                            <li>Cliquer sur l'entité</li>
+                            <li>Dans le formulaire, cliquer sur le bouton <b>modifier</b> (<span class="text-blue q-gutter-md" style="font-size: 1.5em"><q-icon name="sym_o_edit" style="margin-top: 0;"/></span>)</li>
+                            <li>Mettre les données de l'entité à jour dans le formulaire</li>
+                            <li>Cliquer sur <b>enregistrer</b> (<span class="text-blue q-gutter-md" style="font-size: 1.5em"><q-icon name="sym_o_save" style="margin-top: 0;"/></span>), puis sur <b>fermer</b> (<span class="text-blue q-gutter-md" style="font-size: 1.5em"><q-icon name="sym_o_close" style="margin-top: 0;"/></span>)</li>
+                        </ol>
+                    </div>
+
+                    <div class="block">
+                        Activer / désactiver une personne ou un groupe parlementaire
+                        <ol>
+                            <li>Cliquer sur le bouton d'état (<span class="text-blue q-gutter-md" style="font-size: 1.5em"><q-icon name="sym_o_switches" style="margin-top: 0;"/></span>) dans le tableau des parlementaires</li>
+                        </ol>
+                    </div>
+                </div>
+            </p>
+
+            <video class="videoFormat" controls>
+                <source src="video/parlementaire_edition.mp4" type="video/mp4" />
+            </video>
+        </template>
+    </FormSection>
+
     <!-- </template>
     </Form> -->
 </template>
@@ -80,4 +119,16 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.videoFormat{
+    width: 500px;
+}
+.row {
+  width: 100%;
+  margin: 0 auto;
+}
+.block {
+  width: 550px;
+  display: inline-block;
+}
+</style>
