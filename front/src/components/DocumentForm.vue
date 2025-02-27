a69b0524fdabdcb00f118cb3c9d9af5e2f289cc1
Matthew Parkan <matthew.parkan@gmail.com>
2025-02-27 17:07:28 +0100

    Updated document form

--- front/src/components/DocumentForm.vue
+++ front/src/components/DocumentForm.vue
@@ -4,8 +4,42 @@
         <template v-slot:body>
 
             <!-- FILE SECTION -->
-            <FormSection title="Informations générales" class="q-mt-none">
+            <FormSection title="" class="q-mt-none">
                 <template v-slot:content>
+
+
+                    <!-- FILE SELECTOR FIELD -->
+                    <div class="col q-my-md" v-if="!document.file">
+                        <!-- counter max-files="1" -->
+                        <q-file bg-color="white" outlined v-model="newFile" label="Sélectionner un fichier" :rules="[v => checkFile(v)]" clearable @update:model-value="getFileAttributes">
+                            <template v-slot:prepend>
+                                <q-icon name="sym_o_attach_file" />
+                            </template>
+                        </q-file>
+                    </div>
+
+                    <!-- ATTACHED FILE CARD -->
+                    <q-card class="col q-my-md" flat bordered v-if="document.file">
+
+                        <q-item>
+                            <q-item-section avatar>
+                                <q-avatar color="blue" text-color="white" icon="sym_o_file_present" />
+                            </q-item-section>
+
+                            <q-item-section>
+                                <q-item-label class="text-weight-medium"><a :href="fileDownloadUrl">{{ document.filename }}</a></q-item-label>
+                                <q-item-label caption>{{ formatBytes(document.size) }}</q-item-label>
+                            </q-item-section>
+
+                            <q-item-section avatar>
+                                <q-btn dense round flat color="red" name="delete" @click="showDeleteDialog(document)" icon="sym_o_delete">
+                                    <q-tooltip class="bg-black">Supprimer</q-tooltip>
+                                </q-btn>
+                            </q-item-section>
+                        </q-item>
+
+                    </q-card>
+
 
                     <div class="row q-col-gutter-lg q-py-md">
 
@@ -23,18 +57,10 @@
 
                     <div class="row q-col-gutter-lg q-py-md">
 
-                        <!-- FILE SELECTOR FIELD -->
-                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                            <q-file bg-color="white" outlined v-model="newFile" label="Sélectionner un fichier" counter max-files="1" :rules="[v => checkFile(v)]" clearable @update:model-value="getFileAttributes">
-                                <template v-slot:prepend>
-                                    <q-icon name="sym_o_attach_file" />
-                                </template>
-                            </q-file>
-                        </div>
-
                         <!-- TYPE SELECT FIELD -->
                         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                            <q-select bg-color="white" outlined v-model="document.type" :options="documentTypes" option-label="name" emit-value label="Type" :rules="[v => checkFilled(v)]" clearable :disable="!edit">
+                            <!-- v-model="entity.type" :options="entityTypes" option-label="name" option-value="id" emit-value map-options label="Type"  -->
+                            <q-select bg-color="white" outlined v-model="document.type" :options="documentTypes" option-label="name" option-value="id" emit-value map-options label="Type" :rules="[v => checkFilled(v)]" clearable :disable="!edit">
                                 <template v-slot:option="scope">
                                     <q-item v-bind="scope.itemProps">
                                         <q-item-section>
@@ -54,33 +80,6 @@
                         </div>
                     </div>
 
-                    <div class="row q-col-gutter-lg q-py-md" v-if="document.file">
-
-                        <!-- ATTACHED FILE -->
-                        <div class="col">
-                            <q-list bordered separator>
-                                <q-item clickable>
-                                    <q-item-section avatar>
-                                        <!-- <q-icon color="primary" name="sym_o_file_present" /> -->
-                                        <q-avatar color="grey" text-color="white" icon="sym_o_file_present" />
-                                    </q-item-section>
-                                    <q-item-section>
-                                        <q-item-label>{{ document.filename }}</q-item-label>
-                                        <q-item-label caption>{{ document.size }}</q-item-label>
-                                    </q-item-section>
-                                    <q-item-section avatar>
-                                        <q-btn dense round flat color="red" name="delete" @click="" icon="sym_o_delete">
-                                            <q-tooltip class="bg-black">Supprimer</q-tooltip>
-                                        </q-btn>
-                                    </q-item-section>
-                                </q-item>
-
-                            </q-list>
-                        </div>
-
-
-                    </div>
-
                     <!-- TODO REMOVE/DEV DISPLAY JSON-->
                     <div class="bg-light-blue-1 q-my-md q-pa-md" v-if="store.dev">
                         <div>document</div>
@@ -93,17 +92,22 @@
         </template>
 
     </Form>
+
+    <!-- DELETE DIALOG -->
+    <DeleteDialog v-model="dialog.deletion" @delete-event="handleDelete" :content="dialog_content" />
+
 </template>
 
 <script>
 import { store } from '../store/store.js'
-import { checkFilled, checkFile } from '../store/shared.js'
+import { checkFilled, checkFile, formatBytes } from '../store/shared.js'
 import Form from "../components/Form.vue"
 import FormSection from "../components/FormSection.vue"
+import DeleteDialog from './DeleteDialog.vue'
 
 export default {
     name: 'DocumentForm',
-    components: { Form, FormSection },
+    components: { Form, FormSection, DeleteDialog },
     props: { 'item_type': Number, 'edit': Boolean, 'modelValue': Object, 'changewatch': { type: Boolean, default: true } },
     emits: ['update:modelValue'],
     setup() {
@@ -113,6 +117,8 @@ export default {
     data() {
         return {
             store,
+            dialog: { deletion: false },
+            dialog_content: undefined,
             documentTypes: [],
             newFile: null,
             valid: null,
@@ -126,13 +132,18 @@ export default {
             set(document) {
                 this.$emit('update:modelValue', document)
             }
+        },
+        fileDownloadUrl() {
+            return `${this.store.host}/api/document/${this.document.uuid}/download/`;
         }
+
     },
     async created() {
         this.documentTypes = await this.store.getDocumentTypes()
         // this.getDocumentTypes()
     },
     methods: {
+        formatBytes,
         checkFilled,
         checkFile,
         validation(val) {
@@ -145,14 +156,16 @@ export default {
                 this.document.file = this.newFile
                 this.document.filename = this.newFile.name
                 this.document.size = this.newFile.size
-                this.document.type = 1 // TODO rmeove this
+                // this.document.type = 1 // TODO rmeove this
             }
-            /*
-            if (this.document.file) {
-                this.document.filename = this.document.file.name
-                this.document.size = this.document.file.size
-            }
-            */
+        },
+        showDeleteDialog(val) {
+            this.dialog_content = `Supprimer définitivement le document '${val.filename}' ?`
+            this.dialog.deletion = true
+        },
+        handleDelete() {
+            this.document.file = null
+            this.newFile = null
         }
         /*
         async getDocumentTypes() {
